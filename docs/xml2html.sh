6e5df765213c965d1fff2ae31a0d385f6670478e
John Harris <harris@nceas.ucsb.edu>
2000-09-27 21:38:33 +0000

    Added     xml2html.sh which will convert all xml documentation to html content.

---
+++ docs/xml2html.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e 
+
+ls -lrt vegPl*.xml | awk '{print $9}' > list
+ls -lrt plantTaxa*.xml | awk '{print $9}' >> list
+
+cat list |
+while read line
+do
+
+echo $line | sed 's/.xml/.html/g' > tmp
+read OUTFILE<tmp
+rm tmp
+
+java -classpath /usr/local/devtools/jdk1.2.2/lib/rt.jar:/usr/local/devtools/jdk1.2.2/lib/dev.jar:/home/computer/harris/java/xml/xalan_1_0_0/xalan.jar:/home/computer/harris/java/xml/xalan_1_0_0/xerces.jar:./ org.apache.xalan.xslt.Process -IN $line  -XSL tableXml2Html.xsl  -out $OUTFILE
+
+
+
+
+done
+
