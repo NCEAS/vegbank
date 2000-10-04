3a317551dcb18f916b8df76c9414c599d6a982e6
John Harris <harris@nceas.ucsb.edu>
2000-10-04 20:22:23 +0000

    Updated table structure for veg plot database.

--- veg_plot/src/dropVegPlotTables.sql
+++ veg_plot/src/dropVegPlotTables.sql
@@ -3,15 +3,23 @@
  */
 
 set echo off
-drop table speciesTaxon;
+drop table citationContributor;
+drop table citation;
+drop table graphic;
+drop table communityType;
+drop table address;
 drop table telephone;
 drop table onlineResource;
 drop table email;
 drop table plotContributor;
 drop table projectContributor;
-drop table party;
-drop table plotmaster;
+drop table vegplotparty;
 drop table project;
-drop view salientPlotMaster;
-
-
+drop table taxonObservation;
+drop table STAND;
+drop table STRATACOMPOSITION;
+drop table NAMEDPLACE;
+drop table PLOTOBSERVATION; 
+drop table STRATA;
+drop table plot;
+drop table interpretation;
--- veg_plot/src/vegPlotTables.sql
+++ veg_plot/src/vegPlotTables.sql
@@ -5,48 +5,69 @@
 
 CREATE TABLE project (
 project_id INT NOT NULL PRIMARY KEY,
-projectName varchar(20),  --with prototype data from TNC this is
-purpose varchar(20),
-samplingMethod varchar(20),
-coverScale INT);
+projectName varchar(20), 
+description varchar(200)
+);
 
 /*
 * Some attributes are missing from this table - in order to capture just the data fields 
 * represented by the prototype TNC data
 */
 
-CREATE TABLE plotMaster (
+CREATE TABLE plot (
 plot_id INT NOT NULL PRIMARY KEY,
 project_id INT NOT NULL,
-authorPlotNum VARCHAR(30),
-previousPlotRecord INT,
-plotDate DATE,
-dateAccuracy INT,
-landOwner VARCHAR(10),
-effortLevel VARCHAR(8),
-standSize INT,
-treePlotSize INT,
+stand_id INT,
+authorPlotCode  VARCHAR(30),
+parentPlot INT,
+namedPlace INT,
 plotType VARCHAR(15),
+samplingMethod VARCHAR(22),
+coverScale NUMBER(22,6),
+treePlotSize INT,
 plotOriginLat NUMBER(22,6),
 plotOriginLong NUMBER(22,6), 
+horizPosAcc NUMBER(22,6),
 plotShape VARCHAR(12),
 plotSize VARCHAR(20),
-horizPosAcc NUMBER(3),
+plotSizeAcc NUMBER(3),
 altValue NUMBER(8),
-vertPosAcc NUMBER(3),
-town VARCHAR(12),
-county VARCHAR(12),
-state VARCHAR(12),
-country VARCHAR(12),
+altPosAcc NUMBER(3),
 slopeAspect NUMBER(22,6),
 slopeGradient NUMBER(22,6),
 slopePosition VARCHAR(20),
 hydrologicRegime VARCHAR(32),
 soilDrainage VARCHAR(32),
-surfGeo VARCHAR(100),
-FOREIGN KEY (project_id) REFERENCES project
+surfGeo VARCHAR(100)
 );
 
+
+CREATE TABLE plotObservation (
+obs_id INT NOT NULL PRIMARY KEY,
+parentPlot INT NOT NULL,
+previousObs INT,
+authorObsCode VARCHAR(30),
+obsStartDate DATE,
+obsStopDate DATE,
+obsDateAcc INT,
+effortLevel VARCHAR(30),
+hardCopyLocation VARCHAR(30)
+);
+
+
+CREATE TABLE stand (
+stand_id INT NOT NULL PRIMARY KEY, 
+standSize VARCHAR(20),
+description VARCHAR(100)
+);
+
+
+CREATE TABLE namedPlace (
+namedPlace_id INT NOT NULL PRIMARY KEY,
+placeName VARCHAR(100),
+placeDesc VARCHAR(100),
+gazateerRef VARCHAR(100)
+);
 
 
 /*
@@ -55,20 +76,51 @@ FOREIGN KEY (project_id) REFERENCES project
 */
 
 
-CREATE TABLE speciesTaxon (
-taxon_id INT NOT NULL PRIMARY KEY,
-plot_id INT NOT NULL,
-originalTaxonName VARCHAR(120),
-originalTaxonSymbol VARCHAR(10),
-authority VARCHAR(32),
-currentTaxonUnit VARCHAR(32),
-cumStrataCoverage NUMBER(4,6),
-stratumType VARCHAR (10),
-percentCover NUMBER(8),
-FOREIGN KEY (plot_id) REFERENCES plotMaster
+CREATE TABLE strata (
+strata_id INT NOT NULL PRIMARY KEY,
+obs_id INT NOT NULL,
+stratumType VARCHAR(32),
+stratumCover NUMBER(8),
+stratumHeight NUMBER(8)
 );
 
-CREATE TABLE party (
+CREATE TABLE strataComposition (
+strataComposition_id INT NOT NULL PRIMARY KEY,
+taxonObservation_id INT NOT NULL,
+strata_id INT NOT NULL,
+percentCover NUMBER(8)
+);
+
+
+CREATE TABLE taxonObservation (
+taxonObservation_id INT NOT NULL PRIMARY KEY,
+obs_id INT NOT NULL,
+authorNameId VARCHAR(32),
+originalAuthority VARCHAR(32),
+cumStrataCoverage NUMBER(8)
+);
+
+CREATE TABLE communityType (
+communityType_id INT NOT NULL PRIMARY KEY,
+obs_id INT NOT NULL,
+classAssociation VARCHAR(32),
+classificationQuality VARCHAR(32),
+startDate DATE,
+stopDate DATE
+);
+
+CREATE TABLE interpretation (
+interpretation_id INT NOT NULL PRIMARY KEY,
+party_id INT NOT NULL,
+taxonObservation_id  INT NOT NULL,
+circumscription  VARCHAR(32),
+role  VARCHAR(32),
+interpDate DATE,
+notes  VARCHAR(32)
+);
+
+
+CREATE TABLE vegPlotParty (
 party_id INT NOT NULL PRIMARY KEY,
 salutation varchar(20),
 givenName varchar(20),
@@ -85,7 +137,7 @@ project_id INT NOT NULL,
 party_id INT NOT NULL,
 roleCode VARCHAR(32),
 FOREIGN KEY (project_id) REFERENCES project,
-FOREIGN KEY (party_id) REFERENCES party
+FOREIGN KEY (party_id) REFERENCES vegPlotParty
 );
 
 
@@ -94,73 +146,88 @@ plotContributor_id INT NOT NULL PRIMARY KEY,
 plot_id INT NOT NULL,
 party_id INT NOT NULL,
 roleCode VARCHAR(32),
-FOREIGN KEY (plot_id) REFERENCES plotMaster,
-FOREIGN KEY (party_id) REFERENCES party
+FOREIGN KEY (plot_id) REFERENCES plot,
+FOREIGN KEY (party_id) REFERENCES vegPlotParty
 );
 
 
 CREATE TABLE telephone (
+telephone_id INT NOT NULL PRIMARY KEY, 
 party_id INT NOT NULL,
 voicePhone  VARCHAR(10),
 voicePhoneExt VARCHAR(7),
 facsimilePhone  VARCHAR(10),
 otherPhone  VARCHAR(10),
 otherPhoneType  VARCHAR(10),
-FOREIGN KEY (party_id) REFERENCES party
+FOREIGN KEY (party_id) REFERENCES vegPlotParty
 );
 
 CREATE TABLE email (
+email_id INT NOT NULL PRIMARY KEY,
 party_id INT NOT NULL,
 emailAddress VARCHAR(32),
-FOREIGN KEY (party_id) REFERENCES party
+FOREIGN KEY (party_id) REFERENCES vegPlotParty
 );
 
 CREATE TABLE onlineResource (
+onlineResource_id INT NOT NULL PRIMARY KEY,
 party_id INT NOT NULL,
 linkage VARCHAR(31),
 protocol VARCHAR(31),
 name VARCHAR(31),
 applicationProfile VARCHAR(31),
 description VARCHAR(31),
-FOREIGN KEY (party_id) REFERENCES party
+FOREIGN KEY (party_id) REFERENCES vegPlotParty
+);
+
+CREATE TABLE address (
+address_id INT NOT NULL PRIMARY KEY,
+party_id INT NOT NULL,
+deliveryPoint VARCHAR(31),
+city VARCHAR(31),
+administrativeArea VARCHAR(31),
+postalCode INT,
+country VARCHAR(31),
+currentFlag INT
 );
 
 
-/*
-* this view was created to consolidate the 'key' fields of the database into a single manageable view
-*/
 
 
-CREATE VIEW salientPlotMaster 
-AS SELECT plot_id, 
-project_id, 
-authorPlotNum, 
-plotOriginLat, 
-plotOriginLong,
-altValue,
-slopeGradient,
-slopePosition,
-hydrologicRegime,
-surfGeo
-FROM plotMaster;
 
-/* Section below is artificial data intended to test the integrity of the 
- * database.  In furture versions this will be broken-out into its own
- * file 
-*/
+CREATE TABLE graphic (
+graphic_id INT NOT NULL PRIMARY KEY,
+plot_id INT NOT NULL,
+browsen VARCHAR(31),
+browsed VARCHAR(31),
+browset VARCHAR(31),
+FOREIGN KEY (plot_id) REFERENCES plot
+);
 
-INSERT into project (project_id, projectName) VALUES (001, 'testProjectOne');
-INSERT into project (project_id, projectName) VALUES (002, 'testProjectTwo');
-INSERT into project (project_id, projectName) VALUES (003, 'tncTestData');
 
-INSERT into plotMaster (plot_id, project_id, authorPlotNum, plotDate) VALUES (001, 001, 999.25, '12-Feb-00');
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (002, 001, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (003, 001, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (004, 001, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (005, 001, 999.25);
 
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (006, 002, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (007, 002, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (008, 002, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (009, 002, 999.25);
-INSERT into plotMaster (plot_id, project_id, authorPlotNum) VALUES (010, 002, 999.25);
+CREATE TABLE citation (
+citation_id INT NOT NULL PRIMARY KEY,
+plot_id INT NOT NULL,
+title VARCHAR(31),
+alternateTitle VARCHAR(31),
+pubDate date,
+edition VARCHAR(31),
+editionDate date,
+seriesName VARCHAR(31),
+issueIdentification VARCHAR(31),
+otherCitationDetails VARCHAR(31),
+page VARCHAR(31),
+ISBN VARCHAR(31),
+ISSN VARCHAR(31),
+FOREIGN KEY (plot_id) REFERENCES plot
+);
+
+CREATE TABLE citationContributor (
+citationContributor_id INT NOT NULL PRIMARY KEY,
+citation_id INT NOT NULL,
+party_id INT NOT NULL,
+roleCode VARCHAR(31),
+FOREIGN KEY (party_id) REFERENCES vegPlotParty,
+FOREIGN KEY (citation_id) REFERENCES citation
+);
