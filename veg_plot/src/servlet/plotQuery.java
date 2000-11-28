8e210b51ba2a45a8a27d539f4cca5588e10809c9
John Harris <harris@nceas.ucsb.edu>
2000-11-28 16:02:29 +0000

    Removed all the database access code from servlet and, instead, using the db access module.

--- veg_plot/src/servlet/plotQuery.java
+++ veg_plot/src/servlet/plotQuery.java
@@ -11,220 +11,148 @@ import java.math.*;
 import java.net.URL;
 
 
+
+/**
+* This class provides an html page where a user can
+* select from a series of criteria to develop a query
+* that can be used to query the national plots database
+*/
 public class plotQuery extends HttpServlet {
 ResourceBundle rb = ResourceBundle.getBundle("plotQuery");
 
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
-
-
-/*next lines are the DB login variables*/
-String driver_class = "oracle.jdbc.driver.OracleDriver";
-String connect_string = "jdbc:oracle:thin:@128.111.220.63:1521:exp";
-String login = "harris";
-String passwd = "use4dev";
-Connection conn=null;
-Statement query = null;
-Statement query2=null;
-Statement query3=null;
-Statement stmt = null;
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException  {
 
 response.setContentType("text/html");
 PrintWriter out = response.getWriter();
+String title =("plotQuery - Interface to the Plots Database");
 
+//compose a very simple html page
 out.println("<html>");
 out.println("<body>");
 out.println("<head>");
-String title =("plotQuery - Interface to the Plots Database");
 out.println("<title>" + title + "</title>");
 out.println("</head>");
 out.println("<body bgcolor=\"white\">");
 out.println("<h3>" + title + "</h3>");
-
 out.println("<p>");
 out.println("<img alt=\"this is not the final image\" src=\"/harris/owlogo.jpg\" border=\"0\">");
 out.println("<p>");
-
-String firstName = request.getParameter("firstname");
-String lastName = request.getParameter("lastname");
-int i=0;
 out.println(rb.getString("requestparams.params-in-req") + "<br>");
 out.println("<br><i>");
 out.println(rb.getString("requestparams.protoDescription")+"<br>");
 out.println("<br></i>");
 out.println("<A HREF=\"http://www.nceas.ucsb.edu/collab/2180/docs/diagrams/prototypeERD.pdf\"><B><FONT SIZE=\"-1\" FACE=\"arial\">Current Plots Data Model</FONT></B></A>");
 out.println("<br></i>");
-
-if (firstName != null || lastName != null) {
-    out.println(rb.getString("requestparams.firstname"));
-    //out.println(" = " + firstName + "<br>");
-   //out.println(rb.getString("requestparams.lastname"));
-    //out.println(" = " + lastName);
-		//for (i = 0; i < 5; i++) {out.println(firstName);} //end for
-
-/*Query the DB*/
-
-try {
-Class.forName(driver_class);
-//out.println("Trying to connect...<br>");
-if( conn == null)
-	conn = DriverManager.getConnection (connect_string, login, passwd);
-	query = conn.createStatement ();
-	query2 = conn.createStatement ();
-	query3= conn.createStatement ();
-	stmt = conn.createStatement ();
-	out.println("Connected to dev.nceas.ucsb.edu:exp.<br>");
-	}
-catch ( Exception e ){out.println("did not connect "+e.getMessage());}
-
-
-
-/*query the database*/
-ResultSet results = null;
-ResultSet obsData=null;
-ResultSet plotData=null;
-String commonName=null;
-String name=null;
-String symbol=null;
-String nameAuthor=null;
-String party=null;
-String orgName=null;
-String family=null;
-int name_usage_id=0;
-int circumId=0;
-int partyId=0;
-String circumAuthor=null;
-String citation=null;
-String  refSpeciesName=null;
-String refType=null;
-String correlationRefSpeciesName=null;
-String correlationTaxon=null;
-String correlationCommonName=null;
-
-String curStatus=null;
-int partyCircumId=-999;
-int correlationId=-999;
-String correlationAuthor=null;
-String convergence=null;
-
-
-int obsId=-999;
-int parentPlot=-99;
-String authorPlotCode=null;
-String surfGeo=null;
-String authNameId=null;
-String classAssociation=null;
-
-/*get the basic name information*/
-try {
-out.println("<b>Selection results:</b> <br>");
-out.println("<b>Author assigned plot name:</b> <br>");
-
-/*insert the table structure here*/
-
-out.println("<TABLE BORDER=\"1\" CELLPADDING=\"0\" CELLSPACING=\"0\" WIDTH=\"650\">");
-
-out.println("<TR>");
-out.println("        <TD></TD>");
-out.println("</TR>");
-
-out.println("<TR>");
-out.println("<TD><B><FONT COLOR=red SIZE=\"-1\" FACE=\"arial\">author's plot name</FONT></B></TD>");
-out.println("<TD><B><FONT COLOR=red  SIZE=\"-1\" FACE=\"arial\">taxon match</FONT></B></TD>");
-out.println("<TD><B><FONT COLOR=red SIZE=\"-1\" FACE=\"arial\">place name</FONT></B></TD>");
-out.println("<TD><B><FONT COLOR=red SIZE=\"-1\" FACE=\"arial\">surficial geology</FONT></B></TD>");
-out.println("<TD><B><FONT COLOR=green SIZE=\"-1\" FACE=\"arial\">community type</FONT></B></A></TD>");
-//out.println("<TD><A HREF=\"NotesDefinition.html\"><B><FONT SIZE=\"-1\"");
-//out.println(" FACE=\"arial\">NOTES</FONT></B></A></TD>");
-//out.println("<TD><B><FONT SIZE=\"-1\" FACE=\"arial\">DEFINITION</FONT></B></TD>");
-out.println("</TR>");
-
-results = query.executeQuery("select obs_id, authorNameId from taxonObservation where authornameid like '%"+firstName+"%'");
-
-while (results.next()) {
-	obsId = results.getInt("obs_id");
-	authNameId = results.getString("authorNameId");
-//	out.println(obsId);
-	
-		obsData = query2.executeQuery("select parentPlot from plotObservation where obs_id="+obsId);
-
-		while (obsData.next()) {
-		parentPlot=obsData.getInt("parentPlot");
-		//out.println(obsId+","+parentPlot);	
-	//	}
-
-
-plotData=query3.executeQuery("select authorplotcode, surfGeo, currentCommunity  from plot where plot_id= "+parentPlot);
-
-			while (plotData.next()) {
-      				 authorPlotCode =plotData.getString("authorplotcode");
-				surfGeo=plotData.getString("surfGeo");
-				classAssociation=plotData.getString("currentCommunity");
-
-/*insert the table data  here*/
-
-out.println("</TR>");
-out.println("<TD><FONT SIZE=\"-1\" FACE=\"arial\">"+authorPlotCode+"</FONT></TD>");
-out.println("<TD><FONT SIZE=\"-1\" FACE=\"arial\">"+authNameId+"</FONT></TD>");
-out.println("<TD><FONT SIZE=\"-1\" FACE=\"arial\">"+authorPlotCode+"</FONT></TD>");
-out.println("<TD><FONT SIZE=\"-1\" FACE=\"arial\">"+surfGeo+"</FONT></TD>");
-out.println("<TD><FONT SIZE=\"-1\" FACE=\"arial\">"+classAssociation+"</FONT></TD>");
-
-
-out.println("</TR>");
-
-
-
-		//	out.println("<i>"+authorPlotCode+"	"+surfGeo+"	"+authNameId+"</i><br>  \n");
-		//	out.println("</i><P>");
-
-
-		} 
-	} //end while
-} //end while
-out.println("</TABLE>");
-
-stmt.close();
-query.close();
-query2.close();
-query3.close();
-conn.close();
-
-
-} //end try
-catch ( Exception e ){out.println("The Query Jumbled"+e.getMessage());}
-
-
-
-
-
-} //end if
- else {
-            out.println(rb.getString("requestparams.no-params"));
-        }
 out.println("<P>");
 out.print("<form action=\"");
-out.print("plotQuery\" ");   /*make sure to have this pointing to the right servlet6*/
+out.print("plotQuery\" ");
 out.println("method=POST>");
-out.println("Enter taxon");
-out.println("<input type=text size=20 name=firstname>");
+
+//set up the taxon specification
+out.println("Enter taxon");  //set up text field for taxon name
+out.println("<input type=text size=28 name=taxon>");
+out.println("<input type=\"radio\" NAME=\"taxonOperation\" Value=\"required\" CHECKED> required");
+out.println("<input type=\"radio\" NAME=\"taxonOperation\" Value=\"optional\" > optional");
+
+
 out.println("<br>");
 out.println("<br>");
+
+
+//set up the community specification
+out.println("Enter community name");  //set up text field for community name
+out.println("<input type=text size=20 name=community>");
+out.println("<input type=\"radio\" NAME=\"commOperation\" Value=\"required\" CHECKED> required");
+out.println("<input type=\"radio\" NAME=\"commOperation\" Value=\"optional\" > optional");
+
+out.println("<br><br> \n");
 out.println("<input type=submit>");
 out.println("</form>");
 out.println("</body>");
 out.println("</html>");
 
 
-    }
+//determine the criteria for the query
+int i=0;
+String taxonName = request.getParameter("taxon");
+String communityName = request.getParameter("community");
+String taxonOperation=request.getParameter("taxonOperation");
+String commOperation=request.getParameter("commOperation");
 
-    public void doPost(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
+//broad check for entries
+if (taxonName != null || communityName != null) {
+	
+	//for now just pass the taxonName to the composeQuery method
+	//change this to include othe attributes later
+	plotQuery g =new plotQuery();  
+	g.composeQuery("taxonName", taxonName);
+	
+	//now using the xml query file created in composeQuery
+	//issue the query using the plotAccess module
+	plotQuery h =new plotQuery();  
+	h.issueQuery();
+	
+	out.println(rb.getString("requestparams.taxonName"));
+	out.println(" = " + taxonName + " "+taxonOperation+"<br>");
+	out.println(rb.getString("requestparams.communityName"));
+	out.println(" = " + communityName+ " "+commOperation+"<br>");
+
+} //end if
+
+else {
+            out.println(rb.getString("requestparams.no-params"));
+} //end else
+}  //end method
+
+
+public void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
         doGet(request, response);
-    }
+} //end method
+    
+ 
+
+/**
+*  Method to compose and print out a query based
+*  on the query options chosen by the user
+*/
+private void composeQuery (String queryElement, String elementString) {
+try {
+//set up the output query file called query.xml	using append mode 
+PrintStream outFile  = new PrintStream(new FileOutputStream("/jakarta-tomcat/webapps/examples/WEB-INF/lib/query.xml", false)); 
+
+//print the query instructions in the xml document
+outFile.println("<?xml version=\"1.0\"?> \n"+       
+	"<!DOCTYPE vegPlot SYSTEM \"plotQuery.dtd\"> \n"+     
+	"<dbQuery> \n"+
+	"<query> \n"+
+	"<queryElement>"+queryElement+"</queryElement> \n"+
+	"<elementString>"+elementString+"</elementString> \n"+
+	"</query> \n"+
+	"</dbQuery>");
+	
+}
+catch (Exception e) {System.out.println("failed in plotQuery.composeQuery" + 
+	e.getMessage());}
+} //end method
+
+
+/**
+*  Method to use dbAccess to issue the query to the database from the xml file
+*  created in the plotQuery.composeQuery method 
+*/
+private void issueQuery () {
+
+//call the plot access module
+dbAccess g =new dbAccess();  
+g.accessDatabase("/jakarta-tomcat/webapps/examples/WEB-INF/lib/query.xml", 
+"/jakarta-tomcat/webapps/examples/WEB-INF/lib/querySpec.xsl", "query");	
+
+}
+
+
+
+
 
 }
