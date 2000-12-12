76e17c1961daf2c96449172953ed660e65ee92b0
John Harris <harris@nceas.ucsb.edu>
2000-12-12 16:41:08 +0000

    Added definitions of attributes stored in the vegetation community component of the database.

---
+++ docs/communityXML2html.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ls -lt community*.xml | awk '{print $9}' > list
+
+cat list |
+while read line
+do
+
+echo $line | sed 's/.xml/.html/g' > tmp
+read OUTFILE<tmp
+rm tmp
+
+
+#update the date attribute in the xml and then write it to the rtest file
+java -classpath /usr/local/devtools/jdk1.2.2/lib/rt.jar:/usr/local/devtools/jdk1.2.2/lib/dev.jar:/home/computer/harris/java/xml/xalan_1_0_0/xalan.jar:/home/computer/harris/java/xml/xalan_1_0_0/xerces.jar:/home/computer/harris/java:./  updateXML $line test
+
+
+#convert to the html
+java -classpath /usr/local/devtools/jdk1.2.2/lib/rt.jar:/usr/local/devtools/jdk1.2.2/lib/dev.jar:/home/computer/harris/java/xml/xalan_1_0_0/xalan.jar:/home/computer/harris/java/xml/xalan_1_0_0/xerces.jar:./ org.apache.xalan.xslt.Process -IN test  -XSL tableXml2Html.xsl  -out $OUTFILE
+
+
+#java org.apache.xalan.xslt.Process -IN $line  -XSL tableXml2Html.xsl  -out $OUTFILE
+
+#move the html files to their respective directories
+mv $OUTFILE ../veg_community/docs/html/
+
+done
+
