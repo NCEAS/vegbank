4511aaac7ba598f58ab973ea13b3ec7ab4ef3b03
John Harris <harris@nceas.ucsb.edu>
2000-12-28 01:47:44 +0000

    Authentication servlet that registers a cookie with the users browser

---
+++ veg_plot/src/servlet/authenticate.java
@@ -0,0 +1,216 @@
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet to perform user authentication for all the 
+ * servlets within a domain.  This servlet will recognize varying 
+ * degrees of authentication for users and will place cookies representing
+ * the authentication level
+ *
+ * @author John Harris <harris02@hotmail.com>
+ */
+
+public class authenticate extends HttpServlet {
+
+    ResourceBundle rb = ResourceBundle.getBundle("LocalStrings");
+    
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+
+//first block will read all the inputs and write them back to the screen
+try {
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	
+	/*
+	//use this function to figure out the type and number of inputs
+	//it is a temporary function - comment out later
+		Enumeration enum =request.getParameterNames();
+		while (enum.hasMoreElements()) {
+			String name = (String) enum.nextElement();
+			String values[] = request.getParameterValues(name);
+			if (values != null) {
+				for (int i=0; i<values.length; i++) {
+					out.println(name +" ("+ i + "): "
+					+values[i]+"; <br>");
+				}
+			}
+		}
+	*/
+	
+	//grab the user name and password
+	String userName=request.getParameter("userName");
+	String passWord=request.getParameter("password");
+	out.println(userName+" "+passWord);
+	
+	//grab the remote host information
+	String remoteHost=request.getRemoteHost();
+	out.println(remoteHost);
+	
+	//validate the name password pair
+	int validate=0;  //0=invalid
+	if (userName.equals("user") && passWord.equals("pass")) {
+		validate=1;
+		out.println("authentication successful");
+		//log the login in the clientLogger
+		//set a cookie
+		Cookie cookie = new Cookie("null", "null");
+		authenticate m =new authenticate();  
+		m.cookieDelegator(cookie, userName, passWord, remoteHost);
+		response.addCookie(m.registeredCookie);
+	
+	
+	}
+	
+	else {
+		validate=0;
+		out.println("invalid login");
+		//log the login attemp in the clientLogger
+		
+		//redirect to the login page
+	}
+
+	
+	}  //end try
+	
+	
+	catch( Exception e ) {System.out.println("servlet failed in: viewData.main second try   "+e.getMessage());}
+
+	    
+//if statement to see if a username and password are being passed to the serv
+
+
+
+   
+
+    
+	    
+	    
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body bgcolor=\"white\">");
+        out.println("<head>");
+
+        String title = rb.getString("cookies.title");
+        out.println("<title>" + title + "</title>");
+        out.println("</head>");
+        out.println("<body>");
+
+	// relative links
+
+        // XXX
+        // making these absolute till we work out the
+        // addition of a PathInfo issue 
+	
+        out.println("<a href=\"/examples/servlets/cookies.html\">");
+        out.println("<img src=\"/examples/images/code.gif\" height=24 " +
+                    "width=24 align=right border=0 alt=\"view code\"></a>");
+        out.println("<a href=\"/examples/servlets/index.html\">");
+        out.println("<img src=\"/examples/images/return.gif\" height=24 " +
+                    "width=24 align=right border=0 alt=\"return\"></a>");
+
+        out.println("<h3>" + title + "</h3>");
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies.length > 0) {
+            out.println(rb.getString("cookies.cookies") + "<br>");
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                out.print("Cookie Name: " + cookie.getName() + "<br>");
+                out.println("  Cookie Value: " + cookie.getValue() +
+			    "<br><br>");
+            }
+        } else {
+            out.println(rb.getString("cookies.no-cookies"));
+        }
+
+        String cookieName = request.getParameter("cookiename");
+        String cookieValue = request.getParameter("cookievalue");
+        if (cookieName != null && cookieValue != null) {
+            Cookie cookie = new Cookie(cookieName, cookieValue);
+            response.addCookie(cookie);
+            out.println("<P>");
+            out.println(rb.getString("cookies.set") + "<br>");
+            out.print(rb.getString("cookies.name") + "  " + cookieName +
+		      "<br>");
+            out.print(rb.getString("cookies.value") + "  " + cookieValue);
+        }
+        
+        out.println("<P>");
+        out.println(rb.getString("cookies.make-cookie") + "<br>");
+        out.print("<form action=\"");
+        out.println("authenticate\" method=POST>");
+        out.print(rb.getString("cookies.name") + "  ");
+        out.println("<input type=text length=20 name=cookiename><br>");
+        out.print(rb.getString("cookies.value") + "  ");
+        out.println("<input type=text length=20 name=cookievalue><br>");
+        out.println("<input type=submit></form>");
+            
+            
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+        throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
+
+/**
+*  Method to register a cookie and set it in the browser
+*/
+public void cookieDelegator (Cookie cookie, String userName, String passWord, 
+	String remoteHost) {
+	ResourceBundle rb = ResourceBundle.getBundle("plotQuery");
+	String cookieName = "xploit";
+	String cookieValue = "001";
+	String cookieAddress = "30.8.11.31";
+	cookie = new Cookie(cookieName, cookieValue);
+	cookie.setMaxAge(3600);  //set cookie for an hour
+	registeredCookie=cookie;
+	
+	/**register the cookie with in the following format:
+	*  userName, password, ipAddress, date, cookieName, cookieAddress
+	*/
+	try {
+		PrintStream clientLog = new PrintStream(new FileOutputStream(
+		rb.getString("requestparams.clientLog"), true));
+		Date date = new Date();
+
+		clientLog.println(userName+"|"+passWord+"|"+remoteHost
+		+"|"+date+"|"+cookieName+"|"+cookieValue+"|"+cookieAddress);
+
+	}
+	catch (Exception e) {System.out.println("failed in authenticate.cookieDelegator"
+		+"trying to access client log" + e.getMessage());}
+	
+}
+public Cookie registeredCookie;
+
+
+/**
+*  Method to check and see if a cookie is valid
+*/
+public void cookieChecker (String cookieName, String cookieValue, String remoteHost) 
+	{
+	ResourceBundle rb = ResourceBundle.getBundle("plotQuery");
+	if (cookieName.equals("xploit") && cookieValue.equals("001")) { 
+		cookieValidFlag=1;
+	}
+	else {
+		cookieValidFlag=0;
+	}
+}
+public int cookieValidFlag=0;  //1=valid
+}
+
+
---
+++ veg_plot/src/servlet/pageDirector.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+*
+* This servlet handles all the requests for html pages and has been implemented
+* in conjunction with the authenticate servlet.  This class will check to see
+* if the browser has a valid cookie and if so will provide the user with the 
+* requested page, otherwise the login page is displayed  
+*/
+public class pageDirector extends HttpServlet {
+
+  public void doGet(HttpServletRequest req, HttpServletResponse res) 
+                               throws ServletException, IOException {
+				       
+// Use a ServletOutputStream since we may pass binary information
+ServletOutputStream out = res.getOutputStream();
+
+//set the content type to html
+String contentType = getServletContext().getMimeType("login.html");
+res.setContentType(contentType);
+
+//get the cookie
+String cookieName = null;
+String cookieValue = null;
+
+Cookie[] cookies = req.getCookies();
+        if (cookies.length > 0) {
+         
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie cookie = cookies[i];
+                
+	//	out.print("Cookie Name: " +cookie.getName()  + "<br>");
+                	cookieName=cookie.getName();
+	//	out.println("  Cookie Value: " + cookie.getValue() +"<br><br>");
+			cookieValue=cookie.getValue();
+            }
+  	} else {
+          //  out.println("cookies.no-cookies");
+}
+
+
+//get the requested page type
+String pageType = req.getParameter("pageType");
+
+//first determine if the browser has a valid cookie
+//out.println("debug flag val "+cookieName+" "+cookieValue);
+if (cookieName != null && cookieValue != null) {
+	authenticate m =new authenticate();  
+	m.cookieChecker(cookieName, cookieValue, "remoteHost");
+	
+//	out.println("debug flag val "+m.cookieValidFlag);
+	if (m.cookieValidFlag==1) {
+		pageDirector n =new pageDirector();  
+		n.pageTranslator(pageType);
+		ViewFile.returnFile(n.pageFileName, out);
+	}
+	
+	else {
+		ViewFile.returnFile("C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\login.html", out);
+	}
+          
+        }
+
+//if not then pass the login page to the browser	
+else {	
+	ViewFile.returnFile("C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\login.html", out);
+}				       
+
+}
+  
+/**
+* Method to send the contents of a file to the outputStream
+*/  
+  public static void returnFile(String fileName, OutputStream out)
+  throws FileNotFoundException, IOException {
+	  //A FileInputStream is for Bytes
+	  FileInputStream fis=null;
+	  try {
+	  
+	  fis = new FileInputStream(fileName);
+	  byte[] buf = new byte[4 * 1024]; //4K buffer
+	  int bytesRead;
+	  while ((bytesRead=fis.read(buf)) != -1) {
+		  out.write(buf, 0, bytesRead);
+	  }
+	  }
+	  finally {
+		  if (fis !=null) fis.close();
+	  }
+  }
+  
+/**
+*  Method to match the requested page type with the file to be returned to the 
+*  browser
+*
+*  @param pageType String - the type of page requsted 
+*/
+public void pageTranslator (String pageType) 
+	{
+	if (pageType.equals("plotQuery")) pageFileName="C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\plotQuery.html";
+	
+	if (pageType.equals("communityQuery")) pageFileName="C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\communityQuery.html";
+	
+	if (pageType.equals("plantQuery")) pageFileName="C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\plantQuery.html";
+	
+	if (pageType.equals("login")) pageFileName="C:\\\\jakarta-tomcat\\webapps\\examples\\WEB-INF\\lib\\login.html";
+	
+	}
+public String pageFileName; 
+	  
+}
