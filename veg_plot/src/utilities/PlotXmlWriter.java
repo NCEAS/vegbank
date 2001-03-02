94b4bdfdd95729e0365f7abf0b1964c233f35e3a
John Harris <harris@nceas.ucsb.edu>
2001-03-02 17:07:08 +0000

    New class to write a plot or plot-related information to an xml file that can be written to the database which will be used by classes that have transformed data from a legacy format to writing to the database.

---
+++ veg_plot/src/utilities/PlotXmlWriter.java
@@ -0,0 +1,376 @@
+/**
+ * 
+ *    Purpose: To write plot data as xml documents that can then be loaded into
+ * 				the plots database 
+ *    Copyright: 2000 Regents of the University of California and the
+ *             National Center for Ecological Analysis and Synthesis
+ *    Authors: John Harris
+ *
+ */
+
+import java.sql.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.net.URL;
+import java.util.Date;
+
+
+public class PlotXmlWriter {
+
+
+//plot specific site information
+Hashtable plotSiteParams = new Hashtable();
+//species specific site information
+Hashtable plotSpeciesParams = new Hashtable();
+//initialize the vector to store the multitude of taxon names, starta, and covers
+// that may be passed to this method -- used in the plotElementMapper method
+Vector taxonNameElements= new Vector();
+Vector strataTypeElements = new Vector();
+Vector coverAmountElements = new Vector();
+
+//plot observation specific information
+Hashtable plotObservationParams = new Hashtable();
+Vector stratumNameElements = new Vector();  //recognized strata names
+Vector stratumHeightElements = new Vector(); //recognized strata heights
+Vector stratumCoverElements = new Vector(); //recognized strata cover vals
+
+
+
+//project specific information
+Hashtable plotProjectParams = new Hashtable();
+//community specific information
+Hashtable plotCommunityParams = new Hashtable();
+// this is the hash that contains all the elements and will be passed to 
+// the writer method
+Hashtable comprehensivePlot = new Hashtable();
+
+//the output string buffer
+StringBuffer output = new StringBuffer(); 
+
+//the universal null value for the plots database
+String nullValue = "-999.25";
+
+
+
+
+
+
+/**
+ * This method will write a comprensive plot data file, which means a data file 
+ * contains all of the data required for an an upload of a 'new' plot to the 
+ * database that 
+ *
+ * @param comprehensivePlot - a hash table that stores all the data required to
+ * write a full plot and which has a structure identical to:
+ *
+ *
+ */
+public void writePlot (Hashtable comprehensivePlot, String outFile)
+{
+
+//decompose the comprehensive plot data hastable
+dataDecomposer(comprehensivePlot);
+
+//prepare the header
+writeHeader();
+
+//write the project related data -- the remaining children nodes will be
+// automatically written
+writeProjectData(plotProjectParams);
+
+//write the footer
+writeFooter();
+
+try {
+
+	PrintStream out = new PrintStream(new FileOutputStream(outFile));
+	out.println(output.toString());
+
+}
+catch (Exception ex) {System.out.println("Error printing the xml file "+ex);
+	System.exit(1);}
+
+}
+
+
+
+
+
+/**
+ * Method to decompose the 'comprehensivePlot' into data to be written to
+ * specific nodes of the xml document
+ *
+ */
+private void dataDecomposer (Hashtable comprehensivePlot)
+{
+
+//grab from the hash the project data
+plotProjectParams =(Hashtable)comprehensivePlot.get("projectParameters");
+
+
+
+//grab the hash that has all the plot site specific parameters
+plotSiteParams = (Hashtable)comprehensivePlot.get("siteParameters");
+
+//grab the hash that has all the observational data
+plotObservationParams = (Hashtable)comprehensivePlot.get("observationParameters");
+stratumNameElements = (Vector)plotObservationParams.get("stratumName"); //names-->vector
+stratumHeightElements=(Vector)plotObservationParams.get("stratumHeight"); //height-->vector
+stratumCoverElements=(Vector)plotObservationParams.get("stratumCover"); //covers-->vector
+
+
+//the species specific attributes -- into vectors
+plotSpeciesParams = (Hashtable)comprehensivePlot.get("speciesParameters");
+taxonNameElements = (Vector)plotSpeciesParams.get("taxonName"); //names-->vector
+strataTypeElements=(Vector)plotSpeciesParams.get("stratum"); //strata-->vector
+coverAmountElements=(Vector)plotSpeciesParams.get("cover"); //covers-->vector
+
+
+
+//the community specific attributes
+plotCommunityParams = (Hashtable)comprehensivePlot.get("communityParameters");
+
+//System.out.println("observation data: "+plotObservationParams.toString());
+//System.out.println("project data: "+plotProjectParams.toString());
+
+
+}
+
+
+
+
+
+/**
+ * Method to write a plot xml header
+ */
+private void writeHeader ()
+{
+output.append("<?xml version=\"1.0\"?> \n"
++"<!DOCTYPE vegPlot SYSTEM \"vegPlot2001cv.dtd\"> \n"
++"<vegPlot> \n");
+}
+
+
+/**
+ * Method to write a plot xml footer
+ */
+private void writeFooter ()
+{
+output.append(" \n"
++"</vegPlot> \n");
+}
+
+
+
+
+/**
+ * Method to write a plot xml header
+ */
+private void writeProjectData (Hashtable plotProjectParams)
+{
+
+output.append(" \n"
++"<project> \n"
++"	<projectName>"+plotProjectParams.get("projectName")+"</projectName> \n"
++"	<projectDescription>"+plotProjectParams.get("projectDescription")+"</projectDescription> \n"
++"	<projectContributor>"+plotProjectParams.get("projectContributor")+"</projectContributor> \n"
++"\n"
+);
+
+//print the site data here
+writeSiteData(plotSiteParams);
+
+//rememeber to end the project
+output.append("	</project> \n"
++"\n"
+);
+
+
+}
+
+
+/**
+ * Method to write a plot site data
+ */
+private void writeSiteData (Hashtable plotSiteParams)
+{
+
+output.append("	<plot> \n"
++"		<authorPlotCode>"+plotSiteParams.get("authorCode")+"</authorPlotCode> \n"
++"		<plotShape>"+plotSiteParams.get("topoShape")+"</plotShape> \n"
++"		<slopeGradient>"+plotSiteParams.get("slopeGradient")+" </slopeGradient> \n"
++"		<slopeAspect>"+plotSiteParams.get("slopeAspect")+" </slopeAspect> \n"
++"		<surfGeo>"+plotSiteParams.get("surfGeo")+" </surfGeo> \n"
++"		<hydrologicRegime>"+plotSiteParams.get("hydrologicRegime")+" </hydrologicRegime> \n"
++"		<topoPosition>"+plotSiteParams.get("topoPosition")+"</topoPosition> \n"
++"		<soilDrainage>"+plotSiteParams.get("soilDrainage")+" </soilDrainage> \n"
++"		<elevationValue>"+plotSiteParams.get("elevationValue")+" </elevationValue> \n"
++"		<origLat>"+plotSiteParams.get("origLat")+"</origLat> \n"
++"		<origLong>"+plotSiteParams.get("origLong")+"</origLong> \n"
++"		<plotSize>"+plotSiteParams.get("plotSize")+"</plotSize> \n"
++"		<state>"+plotSiteParams.get("state")+"</state> \n"
++"		<xCoord>"+plotSiteParams.get("xCoord")+"</xCoord> \n"
++"		<yCoord>"+plotSiteParams.get("yCoord")+"</yCoord> \n"
++"		<coordType>"+plotSiteParams.get("coordType")+"</coordType> \n"
++"\n"
+);
+
+//print the observation data here
+writeObservationData(plotObservationParams);
+
+//end the plot
+output.append("	</plot> \n"
++"\n"
+);
+
+}
+
+/**
+ * Method to write a plot site data
+ */
+private void writeObservationData (Hashtable plotObservationParams)
+{
+output.append("		<plotObservation> \n"
++"			<soilDepth>"+plotObservationParams.get("soilDepth")+"</soilDepth> \n"
++"			<soilType>"+plotObservationParams.get("soilType")+"</soilType> \n"
++"			<percentRockGravel>"+plotObservationParams.get("percentRockGravel")+" </percentRockGravel> \n"
++"			<authorObsCode>"+plotObservationParams.get("authorObsCode")+"</authorObsCode> \n"
+
++"			<perSmallRx>"+plotObservationParams.get("perSmallRx")+"</perSmallRx> \n"
++"			<perSand>"+plotObservationParams.get("perSand")+"</perSand> \n"
++"			<perLitter>"+plotObservationParams.get("perLitter")+"</perLitter> \n"
++"			<perWood>"+plotObservationParams.get("perWood")+"</perWood> \n"
++"			<perBareSoil>"+plotObservationParams.get("perBareSoil")+"</perBareSoil> \n"
++"			<leafPhenology>"+plotObservationParams.get("leafPhenology")+"</leafPhenology> \n"
++"			<leafType>"+plotObservationParams.get("leafType")+"</leafType> \n"
++"			<physioClass>"+plotObservationParams.get("physioClass")+"</physioClass> \n"
++"\n"
+
+
+);
+//write the strata info
+writeStrataData ();
+
+//write the community information
+writeCommunityData ();
+
+//print the species info
+writeSpeciesData (taxonNameElements,  strataTypeElements,  coverAmountElements);
+
+output.append(
+"		</plotObservation> \n"
++"\n"
+);
+}
+
+
+
+/**
+ * Method to write strata related data
+ */
+private void writeStrataData ()
+{
+
+for (int i=0; i<stratumNameElements.size(); i++) {
+
+try {
+ output.append(" \n"
+ +"			<strata> \n"
+ +"				<strataName>"+(String)stratumNameElements.elementAt(i)+"</strataName> \n"
+ +"				<strataHeight>"+stratumHeightElements.elementAt(i)+"</strataHeight> \n"
+ +"				<strataCover>"+stratumCoverElements.elementAt(i)+"</strataCover> \n"
+ +"			</strata> \n"
+ );
+ }
+catch ( Exception e ) {System.out.println("failed in: "
+	+e.getMessage());  e.printStackTrace();}
+
+ 
+}
+}
+
+
+
+/**
+ * Method to write species related data
+ */
+private void writeSpeciesData (Vector taxonNameElements, Vector strataTypeElements, 
+	Vector coverAmountElements)
+{
+
+//for (int i=0; i<taxonNameElements.size(); i++) {
+for (int i=0; i<9; i++) {
+	
+output.append(" \n"
++"			<taxonObservation> \n"
++"				<authNameId>"+taxonNameElements.elementAt(i)+"</authNameId> \n"
++"				<originalAuthority>"+nullValue+"</originalAuthority> \n"
++"				<strataComposition> \n"
++"					<strataType>"+strataTypeElements.elementAt(i)+"</strataType> \n"
++"					<percentCover>"+coverAmountElements.elementAt(i)+"</percentCover> \n"
++"				</strataComposition> \n"
++"			</taxonObservation> \n"
+);
+
+}
+
+}
+
+
+/**
+ * Method to write community related data
+ */
+private void writeCommunityData ()
+{
+
+output.append(" \n"
++"			<communityAssignment> \n"
++"				<communityName>"+plotCommunityParams.get("communityName")+"</communityName> \n"
++"				<CEGLCode>"+plotCommunityParams.get("CEGLCode")+"</CEGLCode> \n"
++"			</communityAssignment> \n"
+);
+
+
+}
+
+
+
+
+
+
+/**
+ * Method to write party related data
+ */
+private void writePartyData (Hashtable plotSiteParams)
+{
+
+
+output.append("<vegPlot>");
+output.append("<project>");
+output.append("<projectName>"+plotSiteParams.get("authorCode")+"</projectName>");
+output.append("<projectDescription> TNC PROJECT: </projectDescription>");
+output.append("  <projectContributor>");
+output.append("		<role>"+nullValue+"</role>");
+output.append("		<party>");
+output.append("			<salutation>"+nullValue+"</salutation>");
+output.append("			<givenName>"+nullValue+"</givenName>");
+output.append("			<surName>"+nullValue+" </surName>");
+output.append("			<organizationName>"+nullValue+"</organizationName>");
+output.append("			<positionName>"+nullValue+"</positionName>");
+output.append("			<hoursOfService>"+nullValue+"</hoursOfService>");
+output.append("			<contactInstructions>"+nullValue+" </contactInstructions>");
+output.append("			<onlineResource>");
+output.append("				<linkage>"+nullValue+"</linkage>");
+output.append("				<protocol>"+nullValue+"</protocol>");
+output.append("				<name>"+nullValue+"</name>");
+output.append("				<applicationProfile>"+nullValue+"</applicationProfile>");
+output.append("				<description>"+nullValue+"</description>");
+output.append("			</onlineResource>");
+
+}
+
+
+
+}
