59cb2c2cbdbd80dfc8e57e2a4cbaa311ec826f6e
John Harris <harris@nceas.ucsb.edu>
2001-05-18 21:06:59 +0000

    added the enhydra jdbc drivers and the enhydra transaction libraries as shell script variables and added these to the class path

--- veg_plot/bin/issue_xml_statement.sh
+++ veg_plot/bin/issue_xml_statement.sh
@@ -42,20 +42,26 @@ fi
 fi
 
 
-ACCESSRESOURCE=../lib/dbAccess.jar
-ACCESSMODULE=../lib/dbAccess.jar
+ACCESSRESOURCE=dbAccess.jar
+ACCESSMODULE=dbAccess.jar
 #postgres jdbc drivers
-PG_JDBC=../lib/jdbc7.0-1.2.jar
-JDBC=../lib/oracleJDBC.jar
-XALAN=../lib/xalan_1_2_2.jar
-XERCES=../lib/xerces_1_3_1.jar
-
+PG_JDBC=jdbc7.0-1.2.jar
+JDBC=oracleJDBC.jar
+XALAN=xalan_1_2_2.jar
+XERCES=xerces_1_3_1.jar
+ENHYDRA=idb.jar
+JAVAX=jta-spec1_0_1.jar
 
 if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
    CLASSPATH=`cygpath --path --unix "$CLASSPATH"`
 fi
 
-CLASSPATH=$PG_JDBC:$JDBC:$XALAN:$XERCES:$ACCESSRESOURCE
+CLASSPATH=$PG_JDBC:$JDBC:$XALAN:$XERCES:$ACCESSRESOURCE:$ENHYDRA:$JAVAX
+
+#for i in *.jar
+#do
+#CLASSPATH=${CLASSPATH}:$i
+#done
 
 ## convert the existing path to windows
 if [ "$OSTYPE" = "cygwin32" ] || [ "$OSTYPE" = "cygwin" ] ; then
@@ -65,5 +71,6 @@ fi
 #java   -cp $CPATH -jar $VCLIENT
 # convert the existing path to windows
 
-
+echo $CLASSPATH
+#java -cp dbAccess.jar;xalan_1_2_2.jar  dbAccess $XMLFILE $STYLESHEET $ACTION
 java -cp $CLASSPATH dbAccess $XMLFILE $STYLESHEET $ACTION 
