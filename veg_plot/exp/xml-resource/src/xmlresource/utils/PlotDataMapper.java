9cffe8693ff238964f6cd8b7794c6d9adc5f9e85
John Harris <harris@nceas.ucsb.edu>
2001-11-01 17:44:42 +0000

    This xml resource pakage contains the code for doing many of the xml-related functions required by the vegbank system.  Again a bunch of this code is already in cvs and will be removed in the near future - at which point I'll check in the cvs log with the class.

---
+++ veg_plot/exp/xml-resource/src/xmlresource/utils/PlotDataMapper.java
@@ -0,0 +1,237 @@
+/**
+ * 
+ *    Purpose: This class is used to transform data into an xml document that
+ * 			can be loaded into the plots database.  Specifically other classes
+ * 			can use this class to store a plot in a series of hashtables that 
+ *		can then be passed to the PlotXmlWriter class for out put of an xml
+ *		document.
+ *    Copyright: 2000 Regents of the University of California and the
+ *             National Center for Ecological Analysis and Synthesis
+ *
+ *
+ *
+ * 		@author @author@ 
+ * 		@version @release@ 
+ *
+ *     '$Author: harris $'
+ *     '$Date: 2001-11-01 17:44:42 $'
+ *     '$Revision: 1.1 $'
+ *
+ *
+ */
+//package vegclient.framework;
+package xmlresource.utils;
+
+import java.sql.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.net.URL;
+import java.util.Date;
+
+
+import xmlresource.utils.*;
+
+public class PlotDataMapper 
+{
+
+
+	//initialize the vector to store the multitude of taxon names, starta, and covers
+	// that may be passed to this method -- used in the plotElementMapper method
+	public Vector taxonNameElements= new Vector();
+	public Vector cumStrataCoverElements= new Vector();
+	public Vector strataTypeElements = new Vector();
+	public Vector coverAmountElements = new Vector();
+
+	public Vector stratumNameElements = new Vector();  //recognized strata names
+	public Vector stratumHeightElements = new Vector(); //recognized strata heights
+	public Vector stratumCoverElements = new Vector(); //recognized strata cover vals
+
+	//project specific information
+	public Hashtable plotProjectParams = new Hashtable();
+	//plot specific site information
+	public Hashtable plotSiteParams = new Hashtable();
+	//plot observation specific information
+	public Hashtable plotObservationParams = new Hashtable();
+	//species specific site information
+	public Hashtable plotSpeciesParams = new Hashtable();
+	//community specific information
+	public Hashtable plotCommunityParams = new Hashtable();
+
+	// this is the hash that contains all the elements and will be passed to 
+	// the writer method
+	public Hashtable comprehensivePlot = new Hashtable();
+
+
+	/**
+	 * make constructor that will populate the hashtable that stores a series of
+	 * plots with the term null so that the plotXmlWriter class that uses this
+	 * hashtable does not blow up when it hits a null
+	 */
+	 public  PlotDataMapper()
+	 {
+		 //System.out.println("constructed: PlotDataMapper");
+		 //fill in the plot site attributes
+		 PlotElements elements = new PlotElements();
+		 elements.buildElementLists();  //weired method
+		 int siteElementNum = elements.elementListSize("site");
+		 //System.out.println(" "+ siteElementNum);
+		 
+		 //fill the site hashtable
+		 for (int i=0; i<siteElementNum; i++)
+		 {
+//			 plotSiteParams.put(elements.getSiteElement().toString(), "null");
+			 //System.out.println( elements.getSiteElement() );
+		 }
+		 
+	 }
+	 
+
+	/**
+	 * This method puts data attributes into a specific hastable depending on the 
+	 * database category and attribute type assignded to the element.  There are a 
+	 * series of hashtables that that can be loaded in this method.  These 
+	 * hashtables are consistent with the dtd used to define a plot; having 4 main 
+	 * categories: 'project', 'site', 'observation', 'species' and 'community' 
+	 * related data. -- maybe add a has called indicies
+	 * These hash tables are used to store the plot data and then are passed to 
+	 * the 'consolidator' method where they are then put into a single 
+	 * 'comprehensive' hastable that can then be passed to the 'PlotXmlWriter' 
+	 * class for printing to a file
+	 *
+	 * @param
+	 * @param
+	 *
+	 * make comments about about the multitude of species-oriented data that may
+	 * come in and how it is handled
+	 *
+	 */
+
+	public void plotElementMapper (String elementValue, String elementName, 
+	String elementCategory )
+	{
+	try
+	{
+	//store project specific data
+	if ( elementCategory.equals("project") ) 
+	 {
+		// System.out.println(elementName+" "+elementValue);
+		plotProjectParams.put(elementName,elementValue);
+	// 	System.out.println(plotProjectParams.toString());
+	 }
+
+
+	//store site specific data
+	else if ( elementCategory.equals("site") ) 
+	 {
+		plotSiteParams.put(elementName,elementValue);
+	 }
+
+
+	//store plot observational data -- which may include strata information
+	else if ( elementCategory.equals("observation") ) 
+	 {
+		//put vectors with the strata info into the hash tables
+		if (elementName.equals("stratumName"))
+		{
+			stratumNameElements.addElement(elementValue);
+			plotObservationParams.put("stratumName",stratumNameElements);
+		}
+		else if (elementName.equals("stratumHeight")) 
+		{
+			elementValue=elementValue.replace('<','l');
+			stratumHeightElements.addElement(elementValue);
+//		System.out.println(elementValue);
+			plotObservationParams.put("stratumHeight",stratumHeightElements);
+		}
+		else if (elementName.equals("stratumCover")) 
+		{
+			elementValue=elementValue.replace('<','l');
+			stratumCoverElements.addElement(elementValue);
+			plotObservationParams.put("stratumCover",stratumCoverElements);
+		}
+		else 
+		{
+			plotObservationParams.put(elementName,elementValue);
+ 		}
+	}
+
+	//store community related data
+	else if ( elementCategory.equals("community") ) 
+	 {
+		plotCommunityParams.put(elementName,elementValue);
+	 }
+
+	//store species related data 
+	else if ( elementCategory.equals("species") ) 
+	 {
+		//store  the incomming species data in a temporary vector
+		//System.out.println(elementValue);
+		if (elementName.equals("taxonName")) 
+		{
+			//System.out.println(elementValue);
+			taxonNameElements.addElement(elementValue);
+			plotSpeciesParams.put(elementName,taxonNameElements);
+		}
+			else if (elementName.equals("cumStrataCover")) 
+		{
+			//System.out.println(elementValue);
+			cumStrataCoverElements.addElement(elementValue);
+			plotSpeciesParams.put(elementName, cumStrataCoverElements);
+		}
+		
+		else if (elementName.equals("stratum")) 
+		{
+			//System.out.println(elementValue);
+			strataTypeElements.addElement(elementValue);
+			plotSpeciesParams.put(elementName,strataTypeElements);
+		}
+		else if (elementName.equals("cover")) 
+		{
+			//System.out.println(elementValue);
+			coverAmountElements.addElement(elementValue);
+			plotSpeciesParams.put(elementName,coverAmountElements);
+		}
+		
+	}
+	else 
+	{
+		System.out.println("unrecognized veg plot category -- "
+		+"PlotDataMapper.plotElementMapper: "+elementCategory);
+	}
+	}
+	catch(Exception e)
+	{
+		System.out.println( "Exception: "+ e.getMessage() );
+	}
+	
+}
+
+
+
+
+
+	/**
+	 * This method will take all the elements of a single plot and put them into a
+	 * single hashtbale that can be passed to the method that writes the plot
+	 * information to file
+	 * 
+	 * @param plotSiteElements - a hashtable contining the site elements of a plot
+	 * with a cardinality of one
+	 *
+	 */
+	public void plotElementConsolidator (Hashtable plotProjectParams, 
+		Hashtable plotSiteParams, Hashtable plotObservationData, Hashtable	
+		plotCommunityParams, Hashtable plotSpeciesParams)
+	{
+		//System.out.println("test: "+plotProjectParams.toString());
+		//combine the plot categories into a single hash table
+		comprehensivePlot.put("siteParameters",plotSiteParams);
+		comprehensivePlot.put("communityParameters",plotCommunityParams);
+		comprehensivePlot.put("speciesParameters",plotSpeciesParams);
+		comprehensivePlot.put("projectParameters",plotProjectParams);
+		comprehensivePlot.put("observationParameters",plotObservationParams);
+	}
+
+}
