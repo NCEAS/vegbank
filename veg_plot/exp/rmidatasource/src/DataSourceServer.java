55eceb160d245e565539c2f574808d84d78af356
John Harris <harris@nceas.ucsb.edu>
2002-02-04 15:46:18 +0000

    Added some debugging output for related to the binding to the rmi registry, I am still having problems with getting this code running on one machine and suspect that the troupble is related to the netcfg of the machine -- not this code.

--- veg_plot/exp/rmidatasource/src/DataSourceServer.java
+++ veg_plot/exp/rmidatasource/src/DataSourceServer.java
@@ -10,6 +10,7 @@ import java.rmi.*;
 public class DataSourceServer 
 {
 	
+	
 	/**
 	 * method to start the server and specifies the plugin name to 
 	 * be used in the 'PlotDataSource' interface
@@ -18,16 +19,31 @@ public class DataSourceServer
 	 {
       if(System.getSecurityManager() == null) 
 			{
+				
+				 System.out.println("DataSourceServer > starting a new security manager: ");
          System.setSecurityManager(new RMISecurityManager());
       }
       try 
 			{
-         //DataSourceServerInterface fi = new DataSourceImpl("TNCPlotsDB");
-         DataSourceServerInterface fi = new DataSourceImpl("TestPlotSourcePlugin");
-				 Naming.rebind("//127.0.0.1/DataSourceServer", fi);
-      } catch(Exception e) 
+				//this is the name (or IP addy) of the server to bind to
+				String server = "127.0.0.1";
+        
+				DataSourceServerInterface fi = new DataSourceImpl("TNCPlotsDB");
+        //DataSourceServerInterface fi = new DataSourceImpl("TestPlotSourcePlugin");
+				
+				//list the bound serveres to this host
+				String[] remoteObjects = Naming.list(server);  
+				for (int c = 0; c < remoteObjects.length; c++) 
+				{    
+					System.out.println("DataSourceServer > names already bound: "
+					+ remoteObjects[c]  );
+				}
+
+				 Naming.rebind("//"+server+"/DataSourceServer", fi);
+      } 
+			catch(Exception e) 
 			{
-         System.out.println("FileServer: "+e.getMessage());
+         System.out.println("DataSourceServer > Exception: "+e.getMessage());
          e.printStackTrace();
       }
    }
