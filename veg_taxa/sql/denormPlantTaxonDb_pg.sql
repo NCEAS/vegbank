40f0c6aab60caafc1a6764ef2cb4e8512b60529f
John Harris <harris@nceas.ucsb.edu>
2002-02-26 22:08:39 +0000

    Fixed the denormalization script to work with the new data model.

--- veg_taxa/sql/denormPlantTaxonDb_pg.sql
+++ veg_taxa/sql/denormPlantTaxonDb_pg.sql
@@ -3,23 +3,45 @@ DROP TABLE veg_taxa_summary;
 create table veg_taxa_summary (
 	plantname_id int,
 	plantconcept_id int,
-	concatenatedName varchar(80),
-	commonName varchar(100),
-	status varchar(12),
+	plantName varchar(230),
+	plantDescription varchar(230),
+	plantnamestatus varchar(23),
+	classsystem varchar(23),
+	plantlevel varchar(23),
+	parentName varchar(230),
 	acceptedSynonym varchar(80),
 	startDate date,
 	stopDate date
 );
 
---assume that the table with the most 
---rows will be the usage table so use
---it as the basis for the veg_taxa_summary
 
-INSERT INTO veg_taxa_summary (plantname_id, concatenatedName, status, plantconcept_id, startDate, stopDate)
- SELECT plantname_id, plantname, partyUsageStatus, plantconcept_id, startDate, stopDate 
- from plantusage where plantname_id > 0;
+--INSERT ALL THE PLANT NAMES
+INSERT INTO veg_taxa_summary 
+(plantname_id, plantName)
+ SELECT plantname_id, plantname from plantname where plantname_id > 0;
+
+--UPDATE THE CONCEPT INFORMATION
+update  veg_taxa_summary 
+ set plantConcept_id = (select plantConcept_id from plantConcept where veg_taxa_summary.plantName_id = plantconcept.plantname_id );
+
+update  veg_taxa_summary 
+ set plantDescription = (select plantDescription from plantConcept where veg_taxa_summary.plantName_id = plantconcept.plantname_id );
+
+update  veg_taxa_summary 
+ set plantnamestatus = (select plantnamestatus from plantusage where veg_taxa_summary.plantName_id = plantusage.plantname_id );
+
+update  veg_taxa_summary 
+ set classsystem = (select classsystem from plantusage where veg_taxa_summary.plantName_id = plantusage.plantname_id );
+
+update  veg_taxa_summary 
+ set plantlevel = (select plantlevel from plantconcept where veg_taxa_summary.plantName_id = plantconcept.plantname_id );
+
+update  veg_taxa_summary 
+ set parentName = (select plantparentname from plantstatus where veg_taxa_summary.plantconcept_id = plantstatus.plantconcept_id );
 
 
+
+/*
 
 update  veg_taxa_summary 
  set acceptedSynonym =
@@ -41,5 +63,8 @@ update  veg_taxa_summary
 
 
 --column  concatenatedName format a22
+
 select SUBSTR(concatenatedName, 0, 28), SUBSTR(acceptedSynonym, 0, 28), SUBSTR(status, 0, 3), startDate from 
  veg_taxa_summary order by acceptedSynonym;
+
+*/
