eba8b38f32cabcb7fc01c6e03210afa1c86095dc
John Harris <harris@nceas.ucsb.edu>
2002-03-05 20:55:47 +0000

    finally figured out why the updates were taking so long: the key's in the denorm tables were of type numeric and the rest of the database keys were of type integer, found it on a fluke, but now the updated take 2 seconds instead of 25 minutes

--- veg_community/sql/createCommunitySummary_pg.sql
+++ veg_community/sql/createCommunitySummary_pg.sql
@@ -2,60 +2,93 @@
  * This sql script creates a snapshot table that holds most the 
  * information that would be used for querying the database
  */
-
-
 drop table commSummary;
 drop SEQUENCE commSummary_id_seq;
 
 
-
 CREATE SEQUENCE commSummary_id_seq;
 CREATE TABLE commSummary (
-COMMSUMMARY_ID  NUMERIC(20) default nextval('commSummary_id_seq'),
-commName VARCHAR(500),
-dateEntered date,
-abiCode VARCHAR(50),
-classCode VARCHAR(50),
-classLevel VARCHAR(50), 
-commDescription VARCHAR(4000), --truncated description
-conceptOriginDate date,  --origin date of the concept
-conceptUpdateDate date, --date that the concept was updated
-parentAbiCode VARCHAR(50), --abicode of the parent
-commconcept_id  NUMERIC(20),
-recognizingParty VARCHAR(50),  -- the party that recognizes the concept
-partyConceptStatus VARCHAR(50), -- status of the party recognition
-parentCommName VARCHAR(500),
-parentCommDescription  VARCHAR(4000),
---add the community name usage stuff here
-CONSTRAINT commSummary_pk PRIMARY KEY (COMMSUMMARY_ID)
+	COMMSUMMARY_ID  NUMERIC(20) default nextval('commSummary_id_seq'),
+	commName VARCHAR(500),
+	dateEntered date,
+	abiCode VARCHAR(50),
+	classCode VARCHAR(50),
+	classLevel VARCHAR(50), 
+	commDescription VARCHAR(2000), --truncated description
+	conceptOriginDate date,  --origin date of the concept
+	conceptUpdateDate date, --date that the concept was updated
+	parentAbiCode VARCHAR(50), --abicode of the parent
+	commconcept_id  integer,
+	recognizingParty VARCHAR(50),  -- the party that recognizes the concept
+	partyConceptStatus VARCHAR(50), -- status of the party recognition
+	parentCommConceptId integer, -- the concept ID if the community in this database
+	parentCommConceptCode varchar(20), -- the abi code of the parent
+	parentCommName VARCHAR(200), -- the name of the parent
+	parentCommDescription  VARCHAR(2000), -- basically the name of the parent
+	CONSTRAINT commSummary_pk PRIMARY KEY (COMMSUMMARY_ID)
 );  
 
 -- LOAD THE SUMMARY TABLE WITH APPROXIMATES
-insert into commSummary (commName, dateEntered, abiCode, classCode, classLevel,
-	commDescription, conceptOriginDate, conceptUpdateDate, parentAbiCode, commConcept_id)
-	select commName.commName, commName.dateEntered, 'null', commConcept.ceglCode, 
-	commConcept.commLevel, commConcept.conceptDescription,
-	'01-JAN-2001', '01-JAN-2001',
-	commConcept.commParent, commConcept.commConcept_id
+insert into commSummary (
+	commName, 
+	dateEntered, 
+	abiCode, 
+	classCode, 
+	classLevel,
+	commDescription, 
+	conceptOriginDate, 
+	conceptUpdateDate, 
+	parentAbiCode, 
+	commConcept_id,
+	parentCommConceptId)
+	select 
+	commName.commName, 
+	commName.dateEntered, 
+	'null', 
+	commConcept.ceglCode, 
+	commConcept.commLevel, 
+	commConcept.conceptDescription,
+	'01-JAN-2001', 
+	'01-JAN-2001',
+	commConcept.commParent, 
+	commConcept.commConcept_id,
+	commConcept.commParent
 	from commName, commConcept
 	where commName.commname_id = commConcept.commname_id;
 
 
 --CREATE INDEXES
 create index commSummary_commconcept_id on commSummary  (commconcept_id);
-create index commPartyConcept_commconcept_id on commSummary  (commconcept_id);
---create index commSummary_parentAbiCode_id on commSummary  (partentAbiCode);
+--create index commPartyConcept_commconcept_id on commSummary  (commconcept_id);
+create index commStatus_commConcept_id on commStatus  (commConcept_id);
+--create index commStatus_commConceptStatus_id on commStatus  (commConceptStatus);
+
 
 --UPDATE THE STATUS FOR THE CONCEPT
 update commSummary
 set  partyConceptStatus =
 (select commconceptstatus from commstatus
-where commStatus.commconcept_id = commSummary.commconcept_id);
+where  commsummary.commconcept_id = commStatus.commconcept_id  );
 
---UPDATE THE PARENT CONCEPT
-update commSummary
-	set  parentCommName = 
-	(select commParent from commConcept 
-	where commConcept.commconcept_id = commSummary.commconcept_id);
+
+--UPDATE THE PARENT CONCEPT NAME, CODE and DESCRIPTION
+update commSummary 
+set  parentCommName = 
+(select conceptdescription from commconcept 
+where commsummary.parentcommconceptid = commconcept.commconcept_id );
+
+update commSummary 
+set  parentCommConceptCode = 
+(select ceglcode from commconcept 
+where commsummary.parentcommconceptid = commconcept.commconcept_id );
+
+update commSummary 
+set  parentCommDescription = 
+(select conceptdescription from commconcept 
+where commsummary.parentcommconceptid = commconcept.commconcept_id );
+
+
+
+
 
 
