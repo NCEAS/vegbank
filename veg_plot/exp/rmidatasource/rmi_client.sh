5d77329746549b76d306217f3a4aa8327345c012
John Harris <harris@nceas.ucsb.edu>
2002-05-16 03:21:04 +0000

    Changed the number and type of arguments so that the collection of them matches the main method of the client class.

--- veg_plot/exp/rmidatasource/rmi_client.sh
+++ veg_plot/exp/rmidatasource/rmi_client.sh
@@ -7,24 +7,27 @@
 #*  Release: @release@
 #*	
 #*  '$Author: harris $'
-#*  '$Date: 2002-03-29 16:22:23 $'
-#* 	'$Revision: 1.3 $'
+#*  '$Date: 2002-05-16 03:21:04 $'
+#* 	'$Revision: 1.4 $'
 ##
 
 
-if [ "$#" -lt 2 ]
+if [ "$#" -ne 4 ]
 then
-  echo "USAGE: rmi_client.sh <host> <Access file> {plot, -all}  "
+  echo "USAGE: rmi_client.sh <host> <Access file> <filetype> <plot>  "
 	echo "  host: the host onwhich the server is running"
 	echo "  Access file: the path and name of the TNC plots file"
-	echo "	optional: the plot name or '-all' "
+	echo "  filetype: the type of plots file: 'tnc', 'vbaccess', 'nativexml'"
+	echo "  plot: the plot name or '-all' "
+	echo "   "
 	exit 1
 fi																				
 
 HOST=$1
 FILE=$2
-PLOT=$3
+TYPE=$3
+PLOT=$4
 
-java -classpath ./lib/xmlresource.jar:./lib/datatranslator.jar:./:./lib/rmidatasource.jar  \
--Djava.security.policy=./lib/policy.txt DataSourceClient $1 $2 $3 
+java -classpath ./lib/xml-apis.jar:./lib/xerces_1_4.jar:./lib/xalan_1_2_2.jar:./lib/xmlresource.jar:./lib/datatranslator.jar:./:./lib/rmidatasource.jar  \
+-Djava.security.policy=./lib/policy.txt DataSourceClient $1 $2 $3 $4 
 
