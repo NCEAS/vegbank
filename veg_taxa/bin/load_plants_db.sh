a5ebf77eddc3b146f5bf1c9466318a2906c655a3
John Harris <harris@nceas.ucsb.edu>
2002-07-02 17:40:10 +0000

    Removed some of the extra ( time conusming ) steps related to the loading of the USDA plants.

--- veg_taxa/bin/load_plants_db.sh
+++ veg_taxa/bin/load_plants_db.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ev  
+#!/bin/sh   
 
 
 # This script will build and load the protoype plant 
@@ -24,19 +24,19 @@ CLASSPATH=$JDBC:$XALAN:$XERCES:$PLANTTAXONMODULE
 
 # gunzip the plant data files (they are)
 # gzip compressed in cvs
-gunzip -r $DATADIR
-cp $INITIALDATA96  $TESTDATA96
-cp $INITIALDATA00  $TESTDATA00
+#gunzip -r $DATADIR
+#cp $INITIALDATA96  $TESTDATA96
+#cp $INITIALDATA00  $TESTDATA00
 
 # convert the plantlists to the xml format that they
 # have to be in in order to load the database
 
-java -Xmx600M  -classpath $CLASSPATH USDAPlantsToXml $TESTDATA96
-mv  outfile.xml $DATADIR/testPlantlst1996.bin.xml
+#java -Xmx600M  -classpath $CLASSPATH USDAPlantsToXml $TESTDATA96
+#mv  outfile.xml /tmp/testPlantlst1996.bin.xml
 
 
 #First load the 1996  test data sets 
-java -Xmx700000000  -classpath $CLASSPATH USDAPlantsLoader $DATADIR/testPlantlst1996.bin.xml
+java -Xmx700M  -classpath $CLASSPATH USDAPlantsLoader outfile.xml
 
 
 
