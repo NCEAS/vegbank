307f59b2eaa2621c950e763ca0145bf9135d18ed
Gabe Farrell <farrell@nceas.ucsb.edu>
2002-12-04 22:40:49 +0000

    added some notes about setting this shell script up so that it runs.

--- veg_plot/src/utilities/coord_transform.sh
+++ veg_plot/src/utilities/coord_transform.sh
@@ -1,36 +1,38 @@
 #!/bin/sh  
 
-if [ $# != 2 ] ; then
-    echo 'Usage: coord_transform.sh infile outfile '
+if [ $# != 3 ] ; then
+    echo 'Usage: coord_transform.sh infile outfilei zone '
     echo ''
-    echo 'coordinate transofmation from utm to ll'
+    echo 'coordinate transformation from utm to ll'
     exit 1
 fi
 
 INFILE=$1
 SPHEROID=clark66
-UTMZONE=17
+UTMZONE=$3
 OUTFILE=$2
 touch $OUTFILE
 rm $OUTFILE
 
 
     
-GISBASE=/home/harris/grass
-GISDBASE=/home/harris/gis
-GISBASE=/home/harris/grass
+GISBASE=/usr/local/grass5
+GISDBASE=/home/farrell/gis
 LOCATION_NAME=test
-GISRC=/home/harris/.grassrc5
+# MAKE SURE THAT THIS VARIABLE IS SET TO THE HOME OF THE USER
+# RUNNING THE SERVLET
+GISRC=/home/farrell/.grassrc5
 GIS_LOCK=81198
 
 export GISBASE
-export GISDBASE
 export LOCATION_NAME
+echo $LOCATION_NAME
+export GISDBASE
 export GISRC
 export GIS_LOCK
 
 
-/home/harris/grass/bin/m.u2ll -d  spheroid=$SPHEROID zone=$UTMZONE input=$INFILE output=$OUTFILE
+/usr/local/grass5/bin/m.u2ll -d  spheroid=$SPHEROID zone=$UTMZONE input=$INFILE output=$OUTFILE
 
 	
 cat $OUTFILE
