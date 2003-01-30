ac4c4f17c72282153cdf916b3ec8ad9478ecdfa7
John Harris <harris@nceas.ucsb.edu>
2003-01-30 17:56:37 +0000

    Shell scripts for loading usda plants 1996/2002

---
+++ veg_taxa/bin/load_plants1996_db.bat
@@ -0,0 +1,7 @@
+java -Xmx600M  -classpath ../lib/xerces_1_3_1.jar;../lib/xalan_1_2_2.jar;../lib/jdbc7.0-1.2.jar;../lib/planttaxonomy.jar USDAPlantsLoader2000 ../taxaData/plantlist1996.xml 1996  
+
+
+
+
+
+
