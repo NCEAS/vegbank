ecefd132d67be1d77bc896fe47411c3451f575cd
Gabe Farrell <farrell@nceas.ucsb.edu>
2003-05-30 23:29:32 +0000

    Add a simple sql running script and split out the preloading step form the database rebuilding step.

---
+++ bin/run_sql.sh
@@ -0,0 +1,21 @@
+#!/bin/sh 
+#
+# Takes a database name and a database create script name as parameters.
+# Drops the database and rebuilds it using the sql script specified.
+#
+# Currently Postgres specific.
+#
+
+DATABASE=$1
+
+shift
+
+until [ -z "$1" ]
+do 
+  psql $DATABASE < $1 
+  echo -n "$1"
+  shift
+done
+
+exit 0
+
