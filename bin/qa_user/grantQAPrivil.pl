ca4af8711a8567806b70cfc2425f148ca818fe70
Gabe Farrell <farrell@nceas.ucsb.edu>
2003-06-02 20:36:01 +0000

    Allow parameters for user and permissions. Work on the sequences also.

--- bin/qa_user/grantQAPrivil.pl
+++ bin/qa_user/grantQAPrivil.pl
@@ -3,6 +3,19 @@
 #List of databases to visit
 @databases =  ('framework','vegbank'); 
 
+$user = $ARGV[0];
+$permissions = $ARGV[1];
+
+if ( $user eq "" )
+{
+  $user = "qa"
+}
+
+if ( $permissions eq "" )
+{
+  $permissions = "SELECT";
+}
+
 foreach $database (@databases) {
 
   # Get the list of tables in database
@@ -12,9 +25,15 @@ foreach $database (@databases) {
   foreach $table (@tables) {
     $table =~ m/^\s*$/ and next;
     $table =~ m/^\s*pg_/ and next;
+    chomp($table);
+
     # run a grant on this table
-    print "Run grant on $table";
-    `psql $database -c "GRANT SELECT ON $table TO qa"`;
+    print "Run grant on $table\n";
+    `psql $database -c "GRANT $permissions ON $table TO lee"`;
+     
+     $sequence = $table . "_id_seq \n";
+     print "Run grant on $sequence";
+    `psql $database -c "GRANT $permissions ON $sequence TO lee"`;
                         
   }
 }
