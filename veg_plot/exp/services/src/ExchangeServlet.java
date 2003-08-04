392f23d290be4fc1a5b8ed0dcdb6f77b5ba4c6fa
John Harris <harris@nceas.ucsb.edu>
2003-08-04 00:37:11 +0000

    Added more documentation to the vegbank webservice code and modified the build file and startup scripts so that they work as planned.  Still need to check the queries with m lee, and write the interface mathed and implementation method to retrieve the nearest place to a plot using the gazateer data.

--- veg_plot/exp/services/src/ExchangeServlet.java
+++ veg_plot/exp/services/src/ExchangeServlet.java
@@ -1,3 +1,29 @@
+/**
+ *    '$RCSfile: ExchangeServlet.java,v $'
+ *    Copyright: 2002 Regents of the University of California and the
+ *              National Center for Ecological Analysis and Synthesis
+ *    Authors: John Harris, Gabe Farrell
+ *    Release: @release@
+ *
+ *   '$Author: harris $'
+ *   '$Date: 2003-08-04 00:37:11 $'
+ *   '$Revision: 1.5 $'
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ */
 package vegbank.publish;
 
 import java.lang.*;
@@ -5,137 +31,125 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 import java.sql.*;
+
 import electric.registry.Registry;
 import electric.registry.RegistryException;
 import examples.publish.IExchange;
-import javax.servlet.*;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 
+/**
+ * Class to be used as a test harness for the vegbank web services subsystem
+ * which allows a user to invoke the public methods available in as vegbank
+ * registered webservices via a web browser.  This class is intended to
+ * illustrate the use of publicly available web services at vegbank.org so that
+ * interested parties can better understand how to integrate vegbank data into
+ * their applications.  This source code is derived from similar examples at:
+ *
+ * http://www.themindelectric.com
+ * http://arcweb.esri.com/arcwebonline/
+ *
+ * <p>Valid parameters are:<br>
+ * querytoken=querystring -- querytoken is a single querystring for which a
+ * query is issued accross many of the attributes stored in the vegbank system
+ * analogous to the "google-like" query for a matching keyword in web pages
+ *
+ */
 public class ExchangeServlet extends HttpServlet 
 {
-
-
- public void init(ServletConfig config) throws ServletException 
-	{
-		System.out.println("ExchangeServlet > init");
-		
-		// Startup the webservice
-		try
-		{
-			Publish.main(null);
-		}
-		catch (Exception e)
-		{
-			System.out.println("Could not start Webservice");
-			e.printStackTrace();
-		}
-		
-		super.init(config);
+  private String GAZATEER_SERVLET; //this is the url to a gazateer
+  private String STYLESHEET; //url of the css stylesheet
+  private String IMAGE_URL; // the location to the image directory
+  private String VEGBANK_SERVICE_URL; // the url of the vegbank wsdl
+  public void init(ServletConfig config) throws ServletException 
+  {
+    System.out.println("ExchangeServlet > init");
+    
+    GAZATEER_SERVLET = "http://dredge:8000/mapplotter/servlet/mapplotter";
+    STYLESHEET = "http://numericsolutions.com/includes/default.css";
+    IMAGE_URL = "http://numericsolutions.com/images/";
+    VEGBANK_SERVICE_URL = "http://localhost:8004/vegbank/exchange.wsdl";
+    super.init(config);
   }
 		
 	/**
-	 * this is the main get method
+	 * handles the doGet method request from the HTTP client
+   * @param - request the HttpServletRequest object 
+   * @param - response the HttpServletResponse object
 	 */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	{
 		try
-		{
-			
+		{		
 			PrintWriter out = response.getWriter();
 			Hashtable h1 = this.parameterHash(request);
 			String queryString = (String)h1.get("querytoken");
-			System.out.println("ExchangeServlet > " + queryString);
+			System.out.println("ExchangeServlet > query string: " + queryString);
 			
     	// bind to web service whose WSDL is at the specified URL
-    	String url = "http://localhost:8004/vegbank/exchange.wsdl";
-			System.out.println("ExchangeServlet > binding to: " + url);
-    	ExchangeInterface exchange = (ExchangeInterface) Registry.bind( url, ExchangeInterface.class );
+    	//String url = "http://localhost:8004/vegbank/exchange.wsdl";
+			System.out.println("ExchangeServlet > binding to: " + VEGBANK_SERVICE_URL);
+    	ExchangeInterface exchange = (ExchangeInterface)Registry.bind(url, ExchangeInterface.class );
 
     	// invoke the web service as if it was a local java object
     	Vector v  = exchange.getPlotAccessionNumber(queryString);
+			System.out.println("ExchangeServlet > service return vector size: " + v.size() );
 	 		this.printResults(v, out);
     }
 		catch (Exception e)
 		{
 			System.out.println("Exception: " + e.getMessage() );
+      e.printStackTrace();
 		}
 	}
 	
-	/**
-	 * method to print the attributes out as html to the browser
+
+  /**
+   * method to print the attributes returned from the web service as an html
+   * page fragment.
+   * @param v -- the vector containing the resultant plots
+   * @param out -- the PrintWriter object to write to the browser
 	 */
-	 private void printResults(Vector v, PrintWriter out)
-	 {
-		 StringBuffer sb = new StringBuffer();
-		 sb.append("<html>");
-		 sb.append("<head>");
-		 sb.append("<link href=\"http://numericsolutions.com/includes/default.css\" type=\"text/css\" rel=\"stylesheet\">");
-		 sb.append("</head>");
-		 sb.append("<body bgcolor=\"white\">");
-		 
-		 sb.append("Number of Unique Matches: " + v.size() +"<br> <br>" );
-		 
-		 sb.append("<table width=\"500\"> \n");
-		 sb.append("<tr>");
-		 sb.append("<span class=\"category\">");
-		 sb.append("<td width=40> PLOT </td> <td width=20>LOCATION</td> <td width=20> STATE </td>");
-		 sb.append("</span>");
-		 sb.append("</tr>");
-		 int row = 0;
-		 for (int i=0; i< v.size() ; i++)
-			{
-				Hashtable h = (Hashtable)v.elementAt(i);
-				String accessionNumber = (String)h.get("accessionNumber");
-				// TOKENIZE THE  ACCESSION NUMBER TO GET THE PLOT ID
-				StringTokenizer st = new StringTokenizer(accessionNumber, ".");
-				String buf = st.nextToken();
-				 buf = st.nextToken();
-				String plotId = st.nextToken();
-				//System.out.println("# " + plotId);
-				String longitude = (String)h.get("longitude");
-				String latitude = (String)h.get("latitude");
-				String state = (String)h.get("state");
-				//System.out.println( h.toString()  );
-				if ( row == 0)
-				{
-					sb.append("<tr bgcolor=\"#C0D3E7\" >");
-					// THE ACCESSION NUMBER AND THE COMPREHENSIVE VIEW 
-					sb.append("<td> <span class=\"itemsmall\">  "+accessionNumber+"  ");
-					sb.append(" <a href=\"/framework/servlet/DataRequestServlet?requestDataFormatType=html&clientType=browser&requestDataType=vegPlot&resultType=full&queryType=simple&plotId="+plotId+"\"><img alt=\"Comprehensive view\" src=\"/vegbank/images/comprehensive_sm.gif\" border=\"0\" align=\"left\"></a>");
-					sb.append(" &nbsp;");
-					sb.append(" <a href=\"/framework/servlet/DataRequestServlet?requestDataFormatType=html&clientType=browser&requestDataType=vegPlot&resultType=summary&queryType=simple&plotId="+plotId+"\"><img alt=\"Comprehensive view\" src=\"/vegbank/images/report_sm.gif\" border=\"0\" align=\"left\"></a>");
-					sb.append(" </span> </td>");
-					
-					sb.append("<td> <span class=\"itemsmall\"> ");
-					sb.append(" <a href=\"/mapplotter/servlet/mapplotter?action=mapsinglecoordinate&longitude="+longitude+"&latitude="+latitude+"\">");
-					sb.append(" <img alt=\"Location\" src=\"/vegbank/images/small_globe.gif\" border=\"0\" align=\"center\"> </a> </span> </td> ");
-					sb.append("<td> <span class=\"itemsmall\"> "+state+"</span> </td> ");
-					row = 1;
-				}
-				else
-				{
-					sb.append("<tr bgcolor=\"white\" >");
-						// THE ACCESSION NUMBER AND THE COMPREHENSIVE VIEW 
-					sb.append("<td> <span class=\"itemsmall\">  "+accessionNumber+"  ");
-					sb.append(" <a href=\"/framework/servlet/DataRequestServlet?requestDataFormatType=html&clientType=browser&requestDataType=vegPlot&resultType=full&queryType=simple&plotId="+plotId+"\"><img alt=\"Comprehensive view\" src=\"/vegbank/images/comprehensive_sm.gif\" border=\"0\" align=\"left\"></a>");
-					sb.append(" &nbsp;");
-					sb.append(" <a href=\"/framework/servlet/DataRequestServlet?requestDataFormatType=html&clientType=browser&requestDataType=vegPlot&resultType=summary&queryType=simple&plotId="+plotId+"\"><img alt=\"Comprehensive view\" src=\"/vegbank/images/report_sm.gif\" border=\"0\" align=\"left\"></a>");
-					sb.append(" </span> </td>");
-					
-					sb.append("<td> <span class=\"itemsmall\"> ");
-					sb.append(" <a href=\"/mapplotter/servlet/mapplotter?action=mapsinglecoordinate&longitude="+longitude+"&latitude="+latitude+"\">");
-					sb.append(" <img alt=\"Location\" src=\"/vegbank/images/small_globe.gif\" border=\"0\" align=\"center\"> </a> </span> </td> ");
-					sb.append("<td> <span class=\"itemsmall\"> "+state+"</span> </td> ");
-					
-					row = 0;
-				}
-				sb.append("</tr>");
-			}
-			sb.append("</table> \n");
-			sb.append("</body>");
-		 sb.append("</html>");
-		 out.println(sb.toString() );
+  private void printResults(Vector v, PrintWriter out) {
+    StringBuffer sb = new StringBuffer();
+    sb.append("<html>");
+    sb.append("<head>");
+    sb.append("<link href=\"http://numericsolutions.com/includes/default.css\" type=\"text/css\" rel=\"stylesheet\">");
+    sb.append("</head>");
+    sb.append("<body bgcolor=\"white\">");
+    sb.append("Number of Unique Matches: " + v.size() +"<br> <br>" );
+		sb.append("<table width=\"500\"> \n");
+    sb.append("<tr>");
+    sb.append("<span class=\"category\">");
+    sb.append("<td width=40> PLOT </td> <td width=20>LOCATION</td> <td width=20> STATE </td>");
+    sb.append("</span>");
+    sb.append("</tr>");
+    int row = 0;
+    for (int i=0; i< v.size() ; i++) {
+      Hashtable h = (Hashtable)v.elementAt(i);
+      String accessionNumber = (String)h.get("accessionNumber");
+      System.out.println("ExchangeServlet > processing results: " + accessionNumber);
+      String plotId = accessionNumber;
+      String longitude = (String)h.get("longitude");
+      String latitude = (String)h.get("latitude");
+      String state = (String)h.get("state");
+      sb.append("<tr bgcolor=\"#C0D3E7\" >");
+      sb.append("<td> <span class=\"itemsmall\">  "+accessionNumber+"  ");
+      sb.append(" </span> </td>");
+		  sb.append("<td> <span class=\"itemsmall\"> ");
+      sb.append(" <a href=\""+GAZATEER_SERVLET+"?action=mapsinglecoordinate&longitude="+longitude+"&latitude="+latitude+"\">");
+      sb.append(" <img alt=\"Location\" src=\""+IMAGE_URL+"small_globe.gif\" border=\"0\" align=\"center\"> </a> </span> </td> ");
+      sb.append("<td> <span class=\"itemsmall\"> "+state+"</span> </td> ");
+			sb.append("</tr>");
+      }
+    sb.append("</table> \n");
+		sb.append("</body>");
+    sb.append("</html>");
+		out.println(sb.toString() );
+    out.close();
 	 }
 	
 		
