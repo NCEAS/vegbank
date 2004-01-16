c88013337a12ce8be48fb3e1aa37413bcd00121a
Mark Anderson <anderson@nceas.ucsb.edu>
2004-01-16 02:09:17 +0000

    The new base Action.

---
+++ src/java/org/vegbank/ui/struts/VegbankAction.java
@@ -0,0 +1,67 @@
+/*
+ *	'$RCSfile: VegbankAction.java,v $'
+ *	Authors: @author@
+ *	Release: @release@
+ *
+ *	'$Author: anderson $'
+ *	'$Date: 2004-01-16 02:09:17 $'
+ *	'$Revision: 1.1 $'
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.vegbank.ui.struts;
+
+import java.sql.*;
+import java.util.*;
+import javax.servlet.http.*;
+
+import org.apache.struts.action.*;
+import org.vegbank.common.Constants;
+import org.vegbank.common.model.WebUser;
+import org.vegbank.common.utility.LogUtility;
+
+
+/**
+ * Provides some basic functionality for all Vegank Action classes.
+ *
+ * @author anderson
+ */
+public abstract class VegbankAction extends Action {
+
+
+	/**
+	 *
+	 */
+	public abstract ActionForward execute(
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response);
+
+	/**
+	 *
+	 */
+	public WebUser getUser(HttpSession session) {
+		return (WebUser)session.getAttribute(Constants.USER_KEY);
+	}
+
+	/**
+	 *
+	 */
+	public ResourceBundle getResourceBundle(String path) {
+		return ResourceBundle.getBundle(path);
+	}
+}
