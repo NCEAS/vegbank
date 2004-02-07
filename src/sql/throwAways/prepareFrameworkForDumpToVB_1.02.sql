ba7a95cde44e5d227f5fb8f8caadecf2a3a32696
Mark Anderson <anderson@nceas.ucsb.edu>
2004-02-07 06:46:34 +0000

    now includes changes to usercertification

--- src/sql/throwAways/prepareFrameworkForDumpToVB_1.02.sql
+++ src/sql/throwAways/prepareFrameworkForDumpToVB_1.02.sql
@@ -1,31 +1,4 @@
 ----------------------------------------------------------------------------
--- CREATE userRegionalExp
-----------------------------------------------------------------------------
-CREATE SEQUENCE userRegionalExp_ID_seq;
-CREATE TABLE userRegionalExp
-(
-    userRegionalExp_ID integer  NOT NULL
-          NOT NULL PRIMARY KEY default nextval('userRegionalExp_ID_seq'),
-    userCertification_ID Integer NOT NULL,
-    region varchar (50) NOT NULL,
-    vegetation varchar (50),
-    floristics varchar (50),
-    nvc_ivc varchar (50)
-                                                                                                                                                            
-);
-
-----------------------------------------------------------------------------
--- Drop unused tables
-----------------------------------------------------------------------------
-drop TABLE user_downloads;
-drop TABLE user_submitted_queries ;
-drop TABLE datafile;
-drop TABLE file_accession;
-drop TABLE graphic;
-drop TABLE user_certification_old;
-
-
-----------------------------------------------------------------------------
 -- Link the user_info and the user_certification table first
 ----------------------------------------------------------------------------
 UPDATE user_certification SET user_id = (SELECT user_id FROM user_info WHERE user_certification.email_address = user_info.email_address);
@@ -55,22 +28,71 @@ alter table usercertification rename column vegbank_expected_uses to vb_intentio
 alter table usercertification rename column plotdb_experience_doc to tools_exp;
 alter table usercertification rename column additional_statements to addl_stmt;
 
-alter table usercertification add column exp_region_a integer;
+alter table usercertification add column exp_region_a varchar (32) NOT NULL;
 alter table usercertification add column exp_region_a_veg integer;
 alter table usercertification add column exp_region_a_flor integer;
 alter table usercertification add column exp_region_a_nvc integer;
 
-alter table usercertification add column exp_region_b integer;
+alter table usercertification add column exp_region_b varchar (32) NOT NULL;
 alter table usercertification add column exp_region_b_veg integer;
 alter table usercertification add column exp_region_b_flor integer;
 alter table usercertification add column exp_region_b_nvc integer;
 
-alter table usercertification add column exp_region_c integer;
+alter table usercertification add column exp_region_c varchar (32) NOT NULL;
 alter table usercertification add column exp_region_c_veg integer;
 alter table usercertification add column exp_region_c_flor integer;
 alter table usercertification add column exp_region_c_nvc integer; 
 
 
+----------------------------------------------------------------------------
+-- CREATE userregionalexp
+----------------------------------------------------------------------------
+CREATE SEQUENCE userregionalexp_id_seq;
+CREATE TABLE userregionalexp
+(
+    userregionalexp_id integer  NOT NULL
+          NOT NULL PRIMARY KEY default nextval('userregionalexp_id_seq'),
+    usercertification_id Integer NOT NULL,
+    region varchar (32) NOT NULL,
+    vegetation integer,
+    floristics integer,
+    nvc_ivc integer
+);
+
+----------------------------------------------------------------------------
+-- Populate  userregionalexp
+----------------------------------------------------------------------------
+-- Actually, we're limiting regional expertise to 3 entries for now.
+-- See usercertification.exp_region_*
+-- INSERT INTO userregionalexp ( usercertification_id, region, vegetation, floristics, nvc_ivc) SELECT usercertification_id, nvc_exp_region_a, nvc_exp_vegetation_a, nvc_exp_floristics_a, nvc_exp_usnvc_a from usercertification;
+
+----------------------------------------------------------------------------
+-- Convert old regional expertise values
+-- Other conversions are possible but there are no records for them.
+----------------------------------------------------------------------------
+UPDATE usercertification SET exp_region_a='asia' WHERE nvc_exp_region_a='Asia';
+UPDATE usercertification SET exp_region_a='us-ak_n-can' WHERE nvc_exp_region_a='US Alaska /';
+UPDATE usercertification SET exp_region_a='uscan-midwest' WHERE nvc_exp_region_a='US/CAN - Mid';
+UPDATE usercertification SET exp_region_a='uscan-ne' WHERE nvc_exp_region_a='US/CAN - NE';
+UPDATE usercertification SET exp_region_a='uscan-nw' WHERE nvc_exp_region_a='US/CAN - NW';
+UPDATE usercertification SET exp_region_a='uscan-rockies' WHERE nvc_exp_region_a='US/CAN - Rocky Mts';
+UPDATE usercertification SET exp_region_a='us-se' WHERE nvc_exp_region_a='US - SE';
+
+-- Drop these obsolete columns
+ALTER TABLE usercertification DROP COLUMN nvc_exp_region_a;
+ALTER TABLE usercertification DROP COLUMN nvc_exp_vegetation_a;
+ALTER TABLE usercertification DROP COLUMN nvc_exp_floristics_a;
+ALTER TABLE usercertification DROP COLUMN nvc_exp_usnvc_a;
+
+----------------------------------------------------------------------------
+-- Populate requested_cert_level to value of 3 
+----------------------------------------------------------------------------
+ALTER TABLE usercertification ADD COLUMN requested_cert_level Integer ;
+UPDATE usercertification SET requested_cert_level=3; 
+
+----------------------------------------------------------------------------
+-- Turn user_info into usr
+----------------------------------------------------------------------------
 alter TABLE user_info rename to usr;
 alter TABLE usr RENAME user_id to usr_id;
 alter TABLE usr DROP COLUMN initial_ip_address;
@@ -88,26 +110,20 @@ ALTER TABLE usr DROP COLUMN zip_code;
 
 
 ----------------------------------------------------------------------------
--- Populate  userregionalexp
-----------------------------------------------------------------------------
-INSERT INTO userregionalexp ( usercertification_id, region, vegetation, floristics, nvc_ivc) SELECT usercertification_id, nvc_exp_region_a, nvc_exp_vegetation_a, nvc_exp_floristics_a, nvc_exp_usnvc_a from usercertification;
-
--- Drop these moved columns
-ALTER TABLE usercertification DROP COLUMN nvc_exp_region_a;
-ALTER TABLE usercertification DROP COLUMN nvc_exp_vegetation_a;
-ALTER TABLE usercertification DROP COLUMN nvc_exp_floristics_a;
-ALTER TABLE usercertification DROP COLUMN nvc_exp_usnvc_a;
-
-----------------------------------------------------------------------------
--- Populate requested_cert_level to value of 3 
-----------------------------------------------------------------------------
-ALTER TABLE usercertification ADD COLUMN requested_cert_level Integer ;
-UPDATE usercertification SET requested_cert_level=3; 
-
-
-----------------------------------------------------------------------------
 -- Populate PARTY_ID to value of 1 ( need to exist in db we populate and need
 -- to be FIXED afterwards, i.e. link to correct party using email_address.
 ----------------------------------------------------------------------------
 ALTER TABLE usr ADD COLUMN party_id Integer ;
 UPDATE usr SET party_id=3; 
+
+----------------------------------------------------------------------------
+-- Drop unused tables
+----------------------------------------------------------------------------
+DROP TABLE user_downloads;
+DROP TABLE user_submitted_queries ;
+DROP TABLE datafile;
+DROP TABLE file_accession;
+DROP TABLE graphic;
+DROP TABLE user_certification_old;
+
+
