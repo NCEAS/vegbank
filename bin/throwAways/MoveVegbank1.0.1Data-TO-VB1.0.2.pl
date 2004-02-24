b382819ed0e5298c0f19d155ed2f8f9f16f1df43
Gabe Farrell <farrell@nceas.ucsb.edu>
2004-02-24 21:49:45 +0000

    A second cut at this

--- bin/throwAways/MoveVegbank1.0.1Data-TO-VB1.0.2.pl
+++ bin/throwAways/MoveVegbank1.0.1Data-TO-VB1.0.2.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl
 
-$dbname=$ARGV[0];
+$dbName=$ARGV[0];
 $username=$ARGV[1];
 
-if ( $dbname eq "" || $username eq "" )
+if ( $dbName eq "" || $username eq "" )
 {
 	print "\n####################################################################\n";
 	print "usage: $0 dbName userName\n";
 	print "\tBoth parameters must be set\n";
-	print "\tdbName: Database target for move of vegbank-1.0 data e.g. vegbank. This dbName will be used to create the new db \n";
+	print "\tdbName: Database target for move of vegbank-1.0.1 data e.g. vegbank.\n";
+	print "\t\tThis dbName will also be used to create the new db \n";
 	print "\tuserName: The database user name. e.g. datauser.\n\n";
 	print "\tFramework will be used to pull user data from.\n\n";
 	print "NOTE: Must be run from the vegbank dir to pick up sql files etc.\n";
@@ -17,16 +18,17 @@ if ( $dbname eq "" || $username eq "" )
 }
 
 print "\n######################################################################\n";
-print "# Dump data from old db.\n"; 
-print `pg_dump -U $username $dbName > oldProduction_1.0.1.bak`;
+print "# Dump data from old db into oldProduction_1.0.1.bak.\n"; 
+print `pg_dump $dbName -U $username > oldProduction_1.0.1.bak`;
 
 print "\n######################################################################\n";
-print "# Create a temp database for old $dbName data\n"; 
-print `createdb  -U $username ProductionTemp`;
+print "# Create a temp database for old $dbName data, drop existing first\n"; 
+print `dropdb -U $username ProductionTemp`;
+print `createdb  -U $username -E UNICODE ProductionTemp`;
 
 print "\n######################################################################\n";
 print "# Restore old 1.0.1 into ProductionTemp \n"; 
-print `pg_restore -U $username -d $dbName oldProduction_1.0.1.bak`;
+print `psql -U $username -d ProductionTemp < oldProduction_1.0.1.bak`;
 
 print "\n######################################################################\n";
 print "# Modify ProductionTemp to fit new schema\n"; 
@@ -34,32 +36,35 @@ print `psql  -U $username ProductionTemp < src/sql/vegbank-changes-1.0.1to1.0.2_
 
 print "\n######################################################################\n";
 print "# Dump ProductionTemp \n"; 
-print `pgdump -aD  -U $username --disable-triggers ProductionTemp > vegbank101_102.sql`;
+print `pg_dump -aD --disable-triggers ProductionTemp > vegbank101_102.sql`;
 
 print "\n######################################################################\n";
 print "# Drop $dbName\n"; 
-print `dbdrop -U $username $dbName`;
+print `dropdb -U $username  $dbName`;
 
 print "\n######################################################################\n";
 print "# Create $dbName\n"; 
-print `dbcreate -U $username $dbName`;
+print `createdb -U $username -E 'UNICODE' $dbName`;
+
+print "\n######################################################################\n";
+print "# Generate fresh SQL\n";
+print `echo vegbank | ant  db_generate_sql`;
 
 print "\n######################################################################\n";
 print "# Create empty $dbName using freshly generate SQL\n";
-pring `echo vegbank | ant  db_generate_sql`;
-print `psql -U $username $dbName < build/src/sql/vegbank-ML.sql`;
+print `psql -U $username -d $dbName < build/src/sql/vegbank-ML.sql`;
 
 print "\n######################################################################\n";
 print "# Create temp fields to drop latter\n"; 
-print `psql -U $username $dbName src/sql/vegbank-changes-1.0.1to1.0.2_pg_createfields2drop.sql`;
+print `psql -U $username $dbName < src/sql/vegbank-changes-1.0.1to1.0.2_pg_createfields2drop.sql`;
 
 print "\n######################################################################\n";
 print "# Populate with modified 1.0.1 to 1.0.2 data\n"; 
-print `pg_restore  -U $username -d $dbName vegbank101_102.sql`;
+print `psql  -U $username -d $dbName < vegbank101_102.sql`;
 
 print "\n######################################################################\n";
 print "# Drop temp fields\n"; 
-print `psql -U $username $dbName < src/sql/vegbank-changes-1.0.1to1.0.2_pg_drop.sql`;
+print `psql -U $username -d $dbName < src/sql/vegbank-changes-1.0.1to1.0.2_pg_drop.sql`;
 
 print "\n######################################################################\n";
 print "# Attempt to load modified (1.0.2) production (1.0.1)  dump into $dbName.\n"; 
@@ -68,6 +73,7 @@ print `psql  -U $username $dbName < vegbank101_102.sql`;
 # Need to run post processing
 # Run Framework to Vegbank
 # Run Throwaways
+# ReSyncSequences and PKs
 
 print "\n######################################################################\n";
 print "#CLEAN UP\n";
