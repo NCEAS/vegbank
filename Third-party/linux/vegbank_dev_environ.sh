de51f84977e93e7c883261a24874705700d4884e
Gabe Farrell <farrell@nceas.ucsb.edu>
2004-03-03 00:27:27 +0000

    Store all environmental variables in one place.

---
+++ Third-party/linux/vegbank_dev_environ.sh
@@ -0,0 +1,9 @@
+# Drop this file into /etc/profile.d for all use to get these settings at login.
+export JAVA_HOME=/usr/java/1.4
+export TOMCAT_HOME=/usr/local/devtools/jakarta-tomcat
+export CATALINA_HOME=/usr/local/devtools/jakarta-tomcat
+export ANT_HOME=/usr/local/ant
+export VEGBANK_HOME=/usr/vegbank
+export VEGTEST_HOME=/usr/vegtest
+export PATH=$PATH:$HOME/bin:$ANT_HOME/bin:$TOMCAT_HOME/bin:$JAVA_HOME/bin:$VEGBANK_HOME/bin
+
