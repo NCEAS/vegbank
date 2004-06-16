b9bf2a7873f032d969c63cb76dd8d7e2153e0e1b
Xianhua Liu <liu@nceas.ucsb.edu>
2004-06-16 19:48:22 +0000

    Initial import

---
+++ nvcrs2/documents/test.java
@@ -0,0 +1,10 @@
+public class test
+{
+	public static void main(String [] args)
+	{
+		String str="proposals";
+		String s=str.trim();
+		s=s.substring(0,s.length()-1);
+		System.out.println(s);
+	}
+}
