ec67d866a9322079b53717bfdd1ac8fc69a516ad
Mark Anderson <anderson@nceas.ucsb.edu>
2004-08-11 18:45:06 +0000

    results go to Map now if bean name = 'map';  Now can use AS in queries

--- src/java/org/vegbank/common/command/GenericCommand.java
+++ src/java/org/vegbank/common/command/GenericCommand.java
@@ -4,8 +4,8 @@
  *	Release: @release@
  *
  *	'$Author: anderson $'
- *	'$Date: 2004-07-13 18:54:19 $'
- *	'$Revision: 1.8 $'
+ *	'$Date: 2004-08-11 18:45:06 $'
+ *	'$Revision: 1.9 $'
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,10 +26,8 @@ package org.vegbank.common.command;
 
 import java.sql.ResultSet;
 import java.text.MessageFormat;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.StringTokenizer;
-import java.util.Vector;
+import java.util.*;
+import java.util.regex.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -52,7 +50,30 @@ import org.vegbank.common.utility.VBObjectUtils;
 
 public class GenericCommand 
 {
+
+/*
+
+select somefield from...
+
+select somefield as newname from...
+
+select (subquery stuff goes here as stuff) as somename from...
+
+*/
+
 	private static Log log = LogFactory.getLog(GenericCommand.class);
+
+
+	//// THIS WORKS 
+	// Caveat: Can't put (parens) in subqueries, or within other (parens)
+	private static Pattern subqueryPattern = Pattern.compile("\\(.*?\\) as ");
+
+	//// OTHER ATTEMPTS
+	//private static Pattern selectParamPattern = Pattern.compile("\\(.*\\) as ?(.*))+ from.*");
+	//private static Pattern selectParamPattern = Pattern.compile("select[ distinct| all]? [[\\(.*\\) as ]?(.*)[,]?]+ from.*");
+	//private static Pattern selectParamPattern = Pattern.compile("select \\(.*\\) as (.*) from.*");
+	//private static Pattern selectParamPattern = Pattern.compile("select[\s]+[distinct|all]?[\s]+[\\(.*\\) as] (.*)|(.*)]+ from.*");
+	//private static Pattern selectParamPattern = Pattern.compile("select [distinct |all ]? ([[.*|\\(.*\\)][,]?]+) from .*");
 
 
 	/* (non-Javadoc)
@@ -79,9 +100,32 @@ public class GenericCommand
 				String whereClauseKey, String beanName, String whereParam)
 				throws Exception {
 
-		String[] arr = new String[1];
+		log.debug("execute(req, select, where, bean, (String)where): " + whereParam);
+		String[] arr;
+		arr = new String[1];
 		arr[0] = whereParam;
-		return execute(request, selectClauseKey, whereClauseKey, beanName, whereParam);
+
+		///////////////////////////////////////////////////////////////////
+		/*
+		// parse CSV
+		if (whereParam.indexOf(",") != -1) {
+			StringTokenizer st = new StringTokenizer(whereParam, ", ");
+			int i=0;
+			arr = new String[st.countTokens()];
+			while (st.hasMoreTokens()) {
+				arr[i] = st.nextToken();
+				log.debug("Adding param " + arr[i]);
+				i++;
+			}
+		} else {
+			arr = new String[1];
+			arr[0] = whereParam;
+		}
+		*/
+		///////////////////////////////////////////////////////////////////
+		
+
+		return execute(request, selectClauseKey, whereClauseKey, beanName, arr);
 	}
 
 	/**
@@ -89,10 +133,12 @@ public class GenericCommand
 	 * from a JSP page directly rather than going through the 
 	 * GenericDispatcherAction.
 	 */
+	/*
 	public static List execute(HttpServletRequest request, String selectClauseKey, 
 				String whereClauseKey, String beanName, Object whereParam)
 				throws Exception {
 
+		log.debug("execute(req, select, where, bean, (Object)where)");
 		log.debug("where param: " + whereParam.toString());
 		log.debug("where param's class: " + whereParam.getClass().toString());
 		String str = whereParam.toString();
@@ -101,13 +147,28 @@ public class GenericCommand
 
 		return execute(request, selectClauseKey, whereClauseKey, beanName, arr);
 	}
+	*/
 
-	public static List execute(String selectClauseKey, 
-				String whereClauseKey, String beanName, Object whereParam)
-				throws Exception {
+
+	/*
+	public static List execute(String selectClauseKey, String whereClauseKey, 
+				String beanName, Object whereParam) throws Exception {
+
+		log.debug("execute(select, where, bean, (Object)where)");
 		HttpServletRequest req = null;
 		return execute(req, selectClauseKey, whereClauseKey, beanName, whereParam);
 	}
+	*/
+
+
+	/*
+	public static List execute(String selectClauseKey, String whereClauseKey, 
+				String beanName, String[] whereParams ) throws Exception
+	{
+		log.debug("execute(select, where, bean, (String[])where)");
+		return execute(null, selectClauseKey, whereClauseKey, beanName, whereParams);
+	}
+	*/
 
 	/**
 	 * Another way to execute a command.  It's easier to call this
@@ -117,6 +178,8 @@ public class GenericCommand
 	public static List execute(HttpServletRequest request, String selectClauseKey, 
 				String whereClauseKey, String beanName, String[] whereParams)
 				throws Exception {
+
+		log.debug("execute(req, select, where, bean, (String[])where)");
 
 		String SQLStatement = getSQLStatement(selectClauseKey, whereClauseKey,  whereParams);
 		DatabaseAccess da = new DatabaseAccess();
@@ -143,12 +206,6 @@ public class GenericCommand
 	}
 
 
-	public static List execute(String selectClauseKey, String whereClauseKey, 
-				String beanName, String[] whereParams ) throws Exception
-	{
-		return execute(null, selectClauseKey, whereClauseKey, beanName, whereParams);
-	}
-
 
 	/**
 	 * Uses parameters passed in via the request to construct a SQL Statement from a 
@@ -173,7 +230,7 @@ public class GenericCommand
 			whereClause = SQLResources.getString(whereClauseKey);
 		}
 		
-		//System.out.println(">>>>>>>>>>" + selectClause + whereClause);
+		//log.debug(">>>>>>>>>>" + selectClause + whereClause);
 		
 		// Message format allows the substitution of '{x}' with Strings from 
 		// a String[] where x is the array index
@@ -197,7 +254,7 @@ public class GenericCommand
 			}
 		}
 		
-		//System.out.println(">>>>>>>>>>" + SQLStatement);
+		//log.debug(">>>>>>>>>>" + SQLStatement);
 		return SQLStatement.toString();
 	}
 	
@@ -218,17 +275,29 @@ public class GenericCommand
 		List col = new Vector();
 		while ( rs.next() )
 		{
-			Object bean = Utility.createObject( VBObjectUtils.DATA_MODEL_PACKAGE +  className);
-			for ( int i=0; i<propNames.size(); i++)
-			{
-				String propName = (String) propNames.get(i);
-				String value = rs.getString(i+1);
-				//System.out.println("####" + bean + " " + propName + " " + value);
-				BeanUtils.copyProperty(bean, propName, value);	
+			if (className.equalsIgnoreCase("map")) {
+				Map map = new HashMap();
+				for ( int i=0; i<propNames.size(); i++)
+				{
+					String propName = (String) propNames.get(i);
+					String value = rs.getString(i+1);
+					log.debug("Mapping property " + propName + " = " + value);
+					map.put(propName, value);	
+				}
+		  		col.add(map);
+
+			} else {
+				Object bean = Utility.createObject( VBObjectUtils.DATA_MODEL_PACKAGE +  className);
+				for ( int i=0; i<propNames.size(); i++)
+				{
+					String propName = (String) propNames.get(i);
+					String value = rs.getString(i+1);
+					log.debug("Setting bean property " + propName + " = " + value);
+					BeanUtils.copyProperty(bean, propName, value);	
+				}
+		  		col.add(bean);
 			}
-		  col.add(bean);
 		}
-		//System.out.println("Got " + col.size() + " " + className);
 		return col;
 	}
 	
@@ -251,39 +320,72 @@ public class GenericCommand
 		Vector results = new Vector();
 		
 		// parse selectClause to get all the fieldNames
+		String attribList = null;
+		selectClause = selectClause.toLowerCase();
+
+		log.debug("SOURCE: " + selectClause);
+		//log.debug("PATTERN: " + selectParamPattern.pattern());
+		log.debug("PATTERN: " + subqueryPattern.pattern());
+		Matcher m = subqueryPattern.matcher(selectClause);
+
+		selectClause = m.replaceAll("");
+		log.debug("New string is: " + selectClause);
+
+		if (m.matches()) {
+			log.debug("MATCHED!");
+			//attribList = m.group(1);
+		} else {
+			log.debug("NO MATCH");
+		}
+
+		/*
+		log.debug("attrib list: " + attribList);
+		log.debug("---------------------------------");
+		*/
+
+
 		StringTokenizer st = new StringTokenizer(selectClause);
-		int indexOfDot, indexOfComma;
+		int indexOfDot, indexOfComma, indexOfAs;
 		
 		while ( st.hasMoreTokens() )
 		{
 			String propertyName = st.nextToken();
-			if ( propertyName.equalsIgnoreCase("SELECT") )
+			if ( propertyName.equals("select") )
 			{
 				// Do Nothing
 			}
-			else if ( propertyName.equalsIgnoreCase("FROM"))
+			else if ( propertyName.equals("from"))
 			{
 				return results; // Nothing more of interest here
 			}
 			else
 			{
 				// Add this token with any trailing ',' removed
+				
+				// Check for a named field
+				log.debug("property name:  " + propertyName);
+				indexOfAs = propertyName.indexOf(" as ");
+				log.debug("as?  " + indexOfAs);
+				if ( indexOfAs != -1 ) {
+					propertyName = propertyName.substring(indexOfAs + 4 );
+				}
+
 				indexOfComma = propertyName.indexOf(',');
 				if ( indexOfComma == -1 )
 				{
 					// no comma so let in whole token
 					indexOfDot = propertyName.indexOf('.');
 					if ( indexOfDot == -1 ) {
-						results.add(propertyName.toLowerCase());
+						results.add(propertyName);
 					} else {
 						// remove all before the dot
-						results.add(propertyName.substring(indexOfDot+1).toLowerCase());
+						results.add(propertyName.substring(indexOfDot+1));
 					}
 				}
 				else
 				{
 					// remove the comma
-					results.add(propertyName.substring(0, indexOfComma ).toLowerCase());
+					results.add(propertyName.substring(0, indexOfComma));
 				}
 			}
 		}	
@@ -302,6 +404,6 @@ public class GenericCommand
 		Vector result = 
 			gc.getPropertyNames("SELECT thisField, thatField, thatTable.inthat FROM xxxxx"); 
 			
-		System.out.println(result);
+		log.debug(result);
 	}
 }
