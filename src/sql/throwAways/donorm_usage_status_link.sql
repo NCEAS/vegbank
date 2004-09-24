eb2f8b709f3bea005d16fe8c68789523f3f2d6ef
Mike Lee <mikelee@unc.edu>
2004-09-24 18:07:50 +0000

    added party_ID to the fix of plantUsage denorm fields

--- src/sql/throwAways/donorm_usage_status_link.sql
+++ src/sql/throwAways/donorm_usage_status_link.sql
@@ -1,3 +1,3 @@
-update commUsage set commstatus_id=(select min(commstatus_id) from commstatus where commstatus.commconcept_id=commUsage.commConcept_ID);
+update commUsage set commstatus_id=(select min(commstatus_id) from commstatus where commstatus.commconcept_id=commUsage.commConcept_ID and commstatus.party_Id=commusage.party_ID);
 
-update plantUsage set plantstatus_id=(select min(plantstatus_id) from plantstatus where plantstatus.plantconcept_id=plantUsage.plantConcept_ID);
+update plantUsage set plantstatus_id=(select min(plantstatus_id) from plantstatus where plantstatus.plantconcept_id=plantUsage.plantConcept_ID and commstatus.party_Id=commusage.party_ID);
