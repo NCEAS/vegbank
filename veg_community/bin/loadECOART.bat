be9c6557dab39535d16161d1cabe8eca325e1bd4
Mike Lee <mikelee@unc.edu>
2004-11-17 22:37:18 +0000

    old old preliminary files we no longer use

--- veg_community/bin/loadECOART.bat
+++
@@ -1,1 +0,0 @@
-java -classpath .;../build/lib/vegcommunity.jar;../../lib/jdbc7.0-1.2.jar;../../lib/vegclass_common.jar;../../lib/vegbank_datamodel.jar org.vegbank.communities.datasource.LoadCommunities %1 %2 
