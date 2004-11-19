bc99e2cf3073084bdffb868fef632d205ea810d2
Mike Lee <mikelee@unc.edu>
2004-11-19 00:35:01 +0000

    sql to add some fields to observation

---
+++ src/sql/denorm/addTop5SppToObservation.sql
@@ -0,0 +1,22 @@
+
+---creating fields ----------------------
+
+ALTER TABLE observation ADD COLUMN topTaxon1Name varchar (255) ;
+
+ALTER TABLE observation ADD COLUMN topTaxon1Cover Float ;
+
+ALTER TABLE observation ADD COLUMN topTaxon2Name varchar (255) ;
+
+ALTER TABLE observation ADD COLUMN topTaxon2Cover Float ;
+
+ALTER TABLE observation ADD COLUMN topTaxon3Name varchar (255) ;
+
+ALTER TABLE observation ADD COLUMN topTaxon3Cover Float ;
+
+ALTER TABLE observation ADD COLUMN topTaxon4Name varchar (255) ;
+
+ALTER TABLE observation ADD COLUMN topTaxon4Cover Float ;
+
+ALTER TABLE observation ADD COLUMN topTaxon5Name varchar (255) ;
+
+ALTER TABLE observation ADD COLUMN topTaxon5Cover Float ;
