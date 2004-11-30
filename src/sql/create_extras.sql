cf914d38d12030ea171dca7fa37bf65d148d98a6
Mike Lee <mikelee@unc.edu>
2004-11-30 23:56:53 +0000

    added default values to 'dateentered' fields on obs,plot,*name

--- src/sql/create_extras.sql
+++ src/sql/create_extras.sql
@@ -1,4 +1,4 @@
--- $Id: create_extras.sql,v 1.1 2004-11-11 21:39:31 anderson Exp $
+-- $Id: create_extras.sql,v 1.2 2004-11-30 23:56:53 mlee Exp $
 -- 
 -- Responsible for creating tables used by the Vegbank app
 -- but not found in the model.
@@ -9,3 +9,14 @@ CREATE TABLE keywords (table_id integer NOT NULL,entity text NOT NULL, keywords 
 CREATE INDEX keywords_table_id_entity_key ON keywords (table_id,entity);
 
 CREATE TABLE keywords_extra (table_id integer NOT NULL,entity text NOT NULL, keywords text);
+
+
+-----default values for fields listed here: ---
+
+alter table plot alter column dateentered set default now();
+
+alter table observation alter column dateentered set default now();
+
+alter table plantname alter column dateentered set default now();
+
+alter table commname alter column dateentered set default now();
