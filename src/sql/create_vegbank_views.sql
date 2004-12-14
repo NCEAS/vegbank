28b1908968f4c0837e0a3a52ba5f15311f63f02b
Mike Lee <mikelee@unc.edu>
2004-12-14 23:34:15 +0000

    added a business rule view and started dropping views before creating them, meaning this sql can always be run to get the latest views definitions.

--- src/sql/create_vegbank_views.sql
+++ src/sql/create_vegbank_views.sql
@@ -1,3 +1,5 @@
+drop view view_party_public;
+
 CREATE VIEW view_party_public AS 
   SELECT party_id, accessioncode, salutation, surname, 
     givenname, middlename,organizationname, 
@@ -14,3 +16,14 @@ CREATE VIEW view_party_public AS
     OR party.party_id IN (SELECT party_id FROM plantStatus)
     OR party.party_ID not IN (select party_ID from usr )
     ;
+
+
+drop view view_busRule_plotsizeshape;
+CREATE VIEW view_busRule_plotsizeshape AS 
+
+  SELECT (project_id), (select projectName from project where project.project_ID=observation.projecT_ID) as projectName, 
+   count(1) as plotcount
+   from plot inner join observation on plot.plot_ID=observation.plot_ID 
+   where ((area is null) and (shape is null or (((shape)<>'Plotless') and (upper(shape) not like 'RELEV%')))) group by project_ID;
+   
+   
