a123ea4e1e3407b4a835eb3ff25cc21892f7c1e6
Mike Lee <mikelee@unc.edu>
2005-02-03 22:59:57 +0000

    added some more default values for startDate fields

--- src/sql/create_extras.sql
+++ src/sql/create_extras.sql
@@ -1,4 +1,4 @@
--- $Id: create_extras.sql,v 1.2 2004-11-30 23:56:53 mlee Exp $
+-- $Id: create_extras.sql,v 1.3 2005-02-03 22:59:57 mlee Exp $
 -- 
 -- Responsible for creating tables used by the Vegbank app
 -- but not found in the model.
@@ -20,3 +20,14 @@ alter table observation alter column dateentered set default now();
 alter table plantname alter column dateentered set default now();
 
 alter table commname alter column dateentered set default now();
+
+ALTER TABLE observationSynonym ALTER COLUMN classStartDate SET default now();
+ALTER TABLE partyMember ALTER COLUMN memberStart SET default now();
+
+ALTER TABLE userDataset ALTER COLUMN datasetStart SET default now();
+ALTER TABLE userDatasetItem ALTER COLUMN itemStart SET default now();
+ALTER TABLE userNotify ALTER COLUMN notifyStart SET default now();
+ALTER TABLE userPermission ALTER COLUMN permissionStart SET default now();
+ALTER TABLE userQuery ALTER COLUMN queryStart SET default now();
+ALTER TABLE userPreference ALTER COLUMN preferenceStart SET default now();
+ALTER TABLE userRecordOwner ALTER COLUMN ownerStart SET default now();
