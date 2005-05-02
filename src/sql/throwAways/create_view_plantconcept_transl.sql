31d6fc3fff3b176df4ee2af83619451406d4212f
Mike Lee <mikelee@unc.edu>
2005-05-02 21:53:44 +0000

    views created in parent directory file: create_vegbank_views.sql, not in this file

--- src/sql/throwAways/create_view_plantconcept_transl.sql
+++
@@ -1,2 +0,0 @@
-drop view view_plantconcept_transl;
-create view view_plantconcept_transl AS SELECT plantconcept.plantconcept_id, plantconcept.plantname_id, plantconcept.plantname, plantconcept.reference_id, ((plantconcept.plantname::text || ' ['::text) || ((( SELECT view_reference_transl.reference_id_transl FROM view_reference_transl WHERE view_reference_transl.reference_id = plantconcept.reference_id))::text)) || ']'::text AS plantconcept_id_transl FROM plantconcept;
