b247bf97becc96493a8bbb84c7db800b80ba815a
Mark Anderson <anderson@nceas.ucsb.edu>
2005-07-11 22:23:39 +0000

    added cookie support; using cookies to remember taxon name type

--- web/includes/utils.js
+++ web/includes/utils.js
@@ -807,6 +807,16 @@ function showTaxonName(toshow,tableid) {
     addClassToDocIfAlsoClass("hidden","taxobs_curr_common",tableid);
 	// then show the one
 	removeClassFromDocIfAlsoClass ("hidden",toshow,tableid);
+
+    // set cookie
+    setCookie('taxon_name', gebid("taxonNameSelect").selectedIndex);
+    //alert('setting cookie: taxon_name: ' + gebid("taxonNameSelect").selectedIndex);
+}
+
+function setTaxonNameSelect(i) {
+    var sel = gebid("taxonNameSelect");
+    if (i != null) { sel.selectedIndex = i - 0; }
+    //alert("setTaxonNameSelect: " + i);
 }
 
 function helpNeedsNewPage() {
@@ -938,3 +948,60 @@ function getEventElement(evt) {
     }
     return elem;
 }
+
+
+/* COOKIES ================================================================= */
+
+function getCookieVal (offset) {  
+    var endstr = document.cookie.indexOf (";", offset);  
+    if (endstr == -1)
+        endstr = document.cookie.length;  
+    return unescape(document.cookie.substring(offset, endstr));
+}
+
+function getCookie (name) {  
+    var arg = name + "=";  
+    var alen = arg.length;  
+    var clen = document.cookie.length;  
+    var i = 0;  
+    while (i < clen) {    
+        var j = i + alen;    
+        if (document.cookie.substring(i, j) == arg) 
+            return getCookieVal (j);    
+        i = document.cookie.indexOf(" ", i) + 1;    
+        if (i == 0) 
+            break;   
+    }  
+    return null;
+}
+
+// 
+// sets a 30 day cookie by default
+// PARAMS: name, value, expires (Date object), path, domain, secure (t/f)
+//
+function setCookie (name, value) {  
+    var expDays = 30;
+    var expDefault = new Date(); 
+    expDefault.setTime(expDefault.getTime() + (expDays*24*60*60*1000));
+
+    var argv = setCookie.arguments;  
+    var argc = setCookie.arguments.length;  
+    var expires = (argc > 2) ? argv[2] : expDefault;  
+    var path = (argc > 3) ? argv[3] : null;  
+    var domain = (argc > 4) ? argv[4] : null;  
+    var secure = (argc > 5) ? argv[5] : false;  
+    document.cookie = name + "=" + escape (value) + 
+    ((expires == null) ? "" : ("; expires=" + expires.toGMTString())) + 
+    ((path == null) ? "" : ("; path=" + path)) +  
+    ((domain == null) ? "" : ("; domain=" + domain)) +    
+    ((secure == true) ? "; secure" : "");
+}
+
+function DeleteCookie (name) {  
+    var exp = new Date();  
+    exp.setTime (exp.getTime() - 1);  
+    var cval = getCookie (name);  
+    document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
+}
+
+
