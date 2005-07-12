5ac4c9ab62011c6a82f0899a58444b2034fd8fac
Mike Lee <mikelee@unc.edu>
2005-07-12 17:53:44 +0000

    adding and removing denorm fields

---
+++ src/sql/throwAways/dropTop5s.sql
@@ -0,0 +1,17 @@
+ALTER TABLE observation DROP COLUMN toptaxon1name CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon1cover CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon2name CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon2cover CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon3name CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon3cover CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon4name CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon4cover CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon5name CASCADE ;
+ALTER TABLE observation DROP COLUMN toptaxon5cover CASCADE ;
+
+-------------------------------------------------------------------
+---------- YOU MUST CREATE VIEWS NOW !!!!!! -----------------------
+-------------------------------------------------------------------
+
+select 'YOU MUST CREATE ALL VEGBANK VIEWS NOW!!!!!!!!!!!!!!!!!! ';
+select 'vegbank/src/sql/create_vegbank_views.sql';
