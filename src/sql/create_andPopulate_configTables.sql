14b7686282dbe2b21518baad7926c05f44b2664d
Mike Lee <mikelee@unc.edu>
2005-07-21 17:33:36 +0000

    added values to show/hide red question mark links to dd

--- src/sql/create_andPopulate_configTables.sql
+++ src/sql/create_andPopulate_configTables.sql
@@ -12,7 +12,13 @@ insert into dba_cookie ( cookieName ,  defaultvalue,  viewname, examplePK, descr
 insert into dba_cookie ( cookieName ,  defaultvalue,  viewname, examplePK, description, sortorder)  values ( 'graphic_stemsize','show','observation_comprehensive',3062,'show graphic of stem sizes on this view' ,4) ;
 insert into dba_cookie ( cookieName ,  defaultvalue,  viewname, examplePK, description, sortorder)  values ( 'graphic_stemsize','show','observation_taxa',3062,'show graphic of stem sizes on this view',6 ) ;
 
+--help bits
+insert into dba_cookie ( cookieName ,  defaultvalue,  viewname, examplePK, sortorder)  values ( 'ddlink','hide','global',0,7 ) ;
+
 insert into dba_cookieLabels ( viewOrCookie , description) values ('graphic_stemsize','A graphical representation of tree stems, using DBH size');
 insert into dba_cookieLabels ( viewOrCookie , description) values ('table_stemsize','A tabular overview of stems for each taxon');
 insert into dba_cookieLabels ( viewOrCookie , description) values ('observation_comprehensive','The Comprehensive Plot View');
 insert into dba_cookieLabels ( viewOrCookie , description) values ('observation_taxa','View of plot(s) showing plants only');
+
+insert into dba_cookieLabels ( viewOrCookie , description) values ('global','Global Settings');
+insert into dba_cookieLabels ( viewOrCookie , description) values ('ddlink','Links to Field Definitions with Red Question Marks');
