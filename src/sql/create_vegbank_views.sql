34417df3c5455fc777ad0925fd226ffedc9ec5ac
Mike Lee <mikelee@unc.edu>
2005-08-17 00:48:59 +0000

    much better way of making sure party is public

--- src/sql/create_vegbank_views.sql
+++ src/sql/create_vegbank_views.sql
@@ -53,17 +53,9 @@ CREATE VIEW view_party_public AS
   SELECT party_id, accessioncode, salutation, surname, 
     givenname, middlename,organizationname, 
     contactinstructions, email 
-    FROM party
+    FROM party LEFT JOIN usr ON party.party_ID=usr.party_ID
    WHERE 
-     party.party_id IN (SELECT note.party_id FROM note)
-    OR party.party_id IN (SELECT party_id FROM observationcontributor)
-    OR party.party_id IN (SELECT party_id FROM classcontributor)
-    OR party.party_id IN (SELECT party_id FROM observationsynonym)
-    OR party.party_id IN (SELECT party_id FROM projectcontributor)
-    OR party.party_id IN (SELECT party_id FROM taxoninterpretation)
-    OR party.party_id IN (SELECT party_id FROM commStatus)
-    OR party.party_id IN (SELECT party_id FROM plantStatus)
-    OR party.party_ID not IN (select party_ID from usr )
+     usr.usr_id is NULL OR usr.permission_type>1
     ;
 
 
