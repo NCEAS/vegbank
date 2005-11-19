6b47912af16de2845b715b4feee07269a9572a80
Mike Lee <mikelee@unc.edu>
2005-11-19 23:02:42 +0000

    New denorm that updates number of plots per metadata item, such as project, plantconcept, etc.  Should be run when adding new plots.

---
+++ src/sql/denorm/denorm-forcemeta.sql
@@ -0,0 +1,22 @@
+
+
+-- count observations per entity:
+UPDATE plantconcept set d_obsCount = (select count(1) from (select observation_ID from 
+  taxoninterpretation, view_notemb_taxonobservation as taxonobservation 
+  where taxonobservation.taxonobservation_ID=taxoninterpretation.taxonobservation_id 
+  AND plantconcept.plantconcept_id=taxoninterpretation.plantconcept_id
+  group by observation_ID ) as foo);
+  
+UPDATE commconcept set d_obsCount = (select count(1) FROM (select observation_ID from
+  comminterpretation, view_notemb_commclass as commclass
+  WHERE commclass.commclass_ID=comminterpretation.commclass_id AND commconcept.commconcept_ID=comminterpretation.commconcept_id
+  GROUP BY  observation_ID) as foo);
+  
+update project set d_obsCount=(select count(1) from view_notemb_observation as observation 
+      where observation.project_ID=project.project_ID);
+
+update party set d_obsCount=(select countallcontrib 
+    from view_browseparty_all_count where view_browseparty_all_count.party_ID=party.party_ID);
+
+UPDATE namedplace set d_obsCount=(select count(1) from place where namedplace.namedplacE_ID = place.namedplace_ID);
+
