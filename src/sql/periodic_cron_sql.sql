27a31d991638e27fc91482147e01beccb5887256
Mike Lee <mikelee@unc.edu>
2006-06-28 16:56:56 +0000

    new sql that should be run "periodically" to remove extraneous datasets as well as the all important VACUUM ANALYZE function.

---
+++ src/sql/periodic_cron_sql.sql
@@ -0,0 +1,21 @@
+
+--get rid of userDatasets that are ANON and OLD
+-- first dataset items:
+DELETE FROM userDatasetItem WHERE userdataset_ID IN 
+   (SELECT userdataset_ID FROM userDataset WHERE (age(datasetStart)>('14 days'::INTERVAL)) AND usr_id IS NULL);
+-- then datasets themselves:
+DELETE FROM userDataSet WHERE (age(datasetStart)>('14 days'::INTERVAL)) AND usr_id IS NULL AND 
+  (select count(1) from userDatasetItem where userDatasetItem.userdataset_ID=userdataset.userdataset_ID) = 0;
+
+--get rid of datasetItems that users have deleted more than a month ago
+DELETE FROM userDatasetItem WHERE userdataset_ID IN 
+   (SELECT userdataset_ID FROM userDataset WHERE (age(datasetStop)>('40 days'::INTERVAL)) and datasetStop is not null AND datasetType <> 'load' );
+-- then datasets themselves:
+DELETE FROM userDataSet WHERE (age(datasetStop)>('40 days'::INTERVAL)) and datasetStop is not null AND datasetType <> 'load' AND 
+  (select count(1) from userDatasetItem where userDatasetItem.userdataset_ID=userdataset.userdataset_ID) = 0;
+
+
+
+
+-- tell the database to update indexes, etc., speeds things up a TON! (or a tonne!)
+VACUUM ANALYZE;
