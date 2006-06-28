641ac7898822b28193412bcb09a508f3d0d469d5
Mike Lee <mikelee@unc.edu>
2006-06-28 23:22:41 +0000

    views now dropped in separate file to allow better manipulation of datamodel

--- src/sql/create_vegbank_views.sql
+++ src/sql/create_vegbank_views.sql
@@ -1,53 +1,5 @@
-DROP VIEW view_plotall_withembargo;
+--- VIEWS MUST BE DROPPED in drop_vegbank_views first!
 
-DROP VIEW view_taxonobs_distinctid_curr_counts_plants;
-
-DROP VIEW view_taxonobs_distinctid_curr_counts;  
-DROP VIEW view_taxonobs_distinctid_curr;
---DROP VIEW view_taxonobs_distinctid_orig;
-
-
-DROP VIEW view_taxonobs_withmaxcover;
-DROP VIEW view_csv_taxonimportance;
-DROP VIEW view_csv_taxonimportance_pre;
-DROP VIEW view_browseparty_all_count_combined;
-
-DROP VIEW view_browseparty_classcontrib_count;
-DROP VIEW view_browseparty_obscontrib_count;
-DROP VIEW view_browseparty_projectcontrib_count;
-
-DROP VIEW view_browseparty_all_count;
-DROP VIEW view_browseparty_all;
-DROP VIEW view_browseparty_obscontrib;
-DROP VIEW view_browseparty_classcontrib;
-DROP VIEW view_browseparty_projectcontrib;
-
-DROP VIEW view_comminterp_more;
-DROP VIEW view_taxoninterp_more;
-DROP VIEW view_observation_transl;
-
-
-DROP VIEW view_notemb_classContributor;
-DROP VIEW view_notemb_commInterpretation;
-DROP VIEW view_notemb_commClass;
-
-DROP VIEW view_notemb_disturbanceObs;
-DROP VIEW view_notemb_soilObs;
-DROP VIEW view_notemb_stemCount;
-DROP VIEW view_notemb_stemLocation;
-DROP VIEW view_notemb_taxonAlt;
-DROP VIEW view_notemb_taxonImportance;
-DROP VIEW view_notemb_taxonInterpretation;
-DROP VIEW view_notemb_taxonObservation;
-DROP VIEW view_notemb_observation;
-DROP VIEW view_notemb_plot;
-
-
-DROP VIEW view_plantconcept_transl;
-DROP VIEW view_commconcept_transl;
-DROP VIEW view_reference_transl ;
-DROP VIEW view_party_transl ;
-drop view view_party_public;
 
 CREATE VIEW view_party_public AS 
   SELECT party.party_id, accessioncode, salutation, surname, 
