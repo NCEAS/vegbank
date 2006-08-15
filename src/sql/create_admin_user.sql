e0d015ad94fd763c144b9231819a1f909c1fc1e4
Chad Berkley <berkley@nceas.ucsb.edu>
2006-08-15 21:48:28 +0000

    re-added a line I accidentally took out

--- src/sql/create_admin_user.sql
+++ src/sql/create_admin_user.sql
@@ -1,2 +1,4 @@
+insert into party (party_id, surname, email) 
+values (100000, 'admin', 'admin@vegbank.org');
 insert into usr (usr_id, party_id, password, permission_type, ticket_count, email_address) 
 values (100000, 100000, '5ffffffffffffff8d7ab5d48fffffffffffffff8effffffffffffffe9ffffffffffffffabffffffffffffffb3fffffffffffffff0ffffffffffffff85ffffffffffffffe7ffffffffffffff824', 15, 2000, 'admin@vegbank.org');
