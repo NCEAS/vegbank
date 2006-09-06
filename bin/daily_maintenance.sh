b268cbc35f125d61a61e92baada468f53bba5b6c
Mike Lee <mikelee@unc.edu>
2006-09-06 21:54:38 +0000

    alternate route of getting files to server that need to be cached as ant isn't cooperating with cron

--- bin/daily_maintenance.sh
+++ bin/daily_maintenance.sh
@@ -2,11 +2,20 @@
 #
 # various bit of maintenance on the database
 
+echo ----------------------------------------------
+echo STARTING THE MAINTENANCE FILE AT:
+date
+
 #run some periodic stuff, like removing old datasets
 psql -U @databaseUser@ @databaseName@ < ~/vegbank/src/sql/periodic_cron_sql.sql
 
 #rebuild cached pages on the homepage with ant:
-ant -f ~/vegbank/web/build.xml cache
+# ant isn't working with cron, so we are just running these manually
+#ant -f ~/vegbank/web/build.xml cache
 
-echo DONE WITH MAINTENANCE
+wget -O @webapps_dir@/vegbank/cache/views/raw/raw_countdata.jsp           http://@machineURL@/vegbank/views/raw/raw_countdata.jsp
+wget -O @webapps_dir@/vegbank/cache/views/raw/raw_recentprojects.jsp      http://@machineURL@/vegbank/views/raw/raw_recentprojects.jsp
+
+echo DONE WITH MAINTENANCE AT:
 date
+echo ----------------------------------------------
