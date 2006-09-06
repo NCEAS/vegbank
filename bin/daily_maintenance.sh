a86703b6069e49c11f7dd33a6290236e439f3e3a
Mike Lee <mikelee@unc.edu>
2006-09-06 20:30:33 +0000

    added DONE echo and date

--- bin/daily_maintenance.sh
+++ bin/daily_maintenance.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 #
-# various bit of maintainence on the database
+# various bit of maintenance on the database
 
 #run some periodic stuff, like removing old datasets
 psql -U @databaseUser@ @databaseName@ < ~/vegbank/src/sql/periodic_cron_sql.sql
 
 #rebuild cached pages on the homepage with ant:
 ant -f ~/vegbank/web/build.xml cache
+
+echo DONE WITH MAINTENANCE
+date
